63d32ac8105e7a3ff4f185c73135a1707dd0a258
Andrew Kenyon <ak@loyaltyangels.com>
2015-08-27 11:12:01 +0100

    renamed models to agens added a potential list of agents

---
+++ app/active.py
@@ -0,0 +1,22 @@
+# We possibly don't need the class path but we'll see
+agents = [
+    # ('slug', 'class name')
+    ('tesco', 'agents.Tesco'),
+    ('nectar', 'Nectar'),
+    ('boots', 'Boots'),
+    ('superdrug', 'Superdrug'),
+    ('shell', 'Shell'),
+    ('starbucks', 'Starbucks'),
+    ('costa', 'costa'),
+    ('avios', 'avios'),
+    ('british-airways', 'BritishAirways'),
+    ('morrisons', 'Morrisons'),
+    ('co-operative', 'CoOperative'),
+    ('kfc', 'KFC'),
+    ('greggs', 'Greggs'),
+    ('nandos', 'Nandos'),
+    ('mothercare', 'Mothercare'),
+    ('house-of-fraser', 'HouseOfFraser'),
+    ('debenhams', 'Debenhams'),
+]
+
