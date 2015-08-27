363caee008f4dbb8a5c715cd55ee89370667b9e0
Andrew Kenyon <ak@loyaltyangels.com>
2015-08-27 18:13:01 +0100

    encryption tests improved

--- app/encyption.py
+++ app/encyption.py
@@ -10,12 +10,16 @@ class AESCipher(object):
         self.key = hashlib.sha256(key).digest()
 
     def encrypt(self, raw):
+        if raw is '':
+            raise TypeError('Cannot encrypt nothing')
         raw = self._pad(raw)
         iv = Random.new().read(AES.block_size)
         cipher = AES.new(self.key, AES.MODE_CBC, iv)
         return base64.b64encode(iv + cipher.encrypt(raw))
 
     def decrypt(self, enc):
+        if enc is '':
+            raise TypeError('Cannot decrypt nothing')
         enc = base64.b64decode(enc)
         iv = enc[:AES.block_size]
         cipher = AES.new(self.key, AES.MODE_CBC, iv)
---
+++ tests/test_encryption.py
@@ -0,0 +1,42 @@
+import json
+import unittest
+
+from app.encyption import AESCipher
+from app.utils import generate_random_key
+
+
+class TestEncryption(unittest.TestCase):
+    def setUp(self):
+        self.key = generate_random_key(32)
+
+    def test_encrypt_decrypt(self):
+        message = 'Message To Encrypt'
+        aes_cipher = AESCipher(key=self.key)
+        cipher_text = aes_cipher.encrypt(message)
+        aes_cipher2 = AESCipher(key=self.key)
+        decrypted_message = aes_cipher2.decrypt(cipher_text)
+        self.assertEqual(message, decrypted_message)
+
+    def test_encrypt_decrypt_dictionary(self):
+        login_credentials = {'username': 'test'}
+        message = json.dumps(login_credentials)
+        aes_cipher = AESCipher(key=self.key)
+        cipher_text = aes_cipher.encrypt(message)
+        aes_cipher2 = AESCipher(key=self.key)
+        decrypted_message = aes_cipher2.decrypt(cipher_text)
+        self.assertEqual(message, decrypted_message)
+        decrypted_credentials = json.loads(decrypted_message)
+        self.assertEqual(login_credentials, decrypted_credentials)
+
+    def test_do_not_allow_encryption_of_none(self):
+        aes_cipher = AESCipher(key=self.key)
+        self.assertRaises(TypeError, aes_cipher.encrypt, '')
+
+    def test_do_not_allow_decryption_of_none(self):
+        aes_cipher = AESCipher(key=self.key)
+        cipher_text = aes_cipher.encrypt('blah')
+        self.assertRaises(TypeError, aes_cipher.decrypt, '')
+
+
+if __name__ == '__main__':
+    unittest.main()
