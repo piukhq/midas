41c3eea02910520f3a5e58248fa40e8ceea98f77
Andrew Kenyon <ak@loyaltyangels.com>
2015-08-27 15:04:53 +0100

    added basic init view, return data in no way final

--- app/active.py
+++ app/active.py
@@ -1,22 +1,24 @@
 # We possibly don't need the class path but we'll see
-agents = [
+from app import agents
+
+AGENTS = [
     # ('slug', 'class name')
-    ('tesco', 'agents.Tesco'),
-    ('nectar', 'Nectar'),
+    # ('tesco', 'agents.Tesco'),
+    # ('nectar', 'Nectar'),
     ('boots', 'Boots'),
-    ('superdrug', 'Superdrug'),
-    ('shell', 'Shell'),
-    ('starbucks', 'Starbucks'),
-    ('costa', 'costa'),
-    ('avios', 'avios'),
-    ('british-airways', 'BritishAirways'),
-    ('morrisons', 'Morrisons'),
-    ('co-operative', 'CoOperative'),
-    ('kfc', 'KFC'),
-    ('greggs', 'Greggs'),
-    ('nandos', 'Nandos'),
-    ('mothercare', 'Mothercare'),
-    ('house-of-fraser', 'HouseOfFraser'),
-    ('debenhams', 'Debenhams'),
+    # ('superdrug', 'Superdrug'),
+    # ('shell', 'Shell'),
+    # ('starbucks', 'Starbucks'),
+    # ('costa', 'costa'),
+    # ('avios', 'avios'),
+    # ('british-airways', 'BritishAirways'),
+    # ('morrisons', 'Morrisons'),
+    # ('co-operative', 'CoOperative'),
+    # ('kfc', 'KFC'),
+    # ('greggs', 'Greggs'),
+    # ('nandos', 'Nandos'),
+    # ('mothercare', 'Mothercare'),
+    # ('house-of-fraser', 'HouseOfFraser'),
+    # ('debenhams', 'Debenhams'),
 ]
 
--- app/views.py
+++ app/views.py
@@ -1,18 +1,39 @@
+from app import app, active
+from flask import request, url_for
 from flask_restful import Resource, Api
-from app import app
-from flask import request
-
 api = Api(app)
 
 
 class Balance(Resource):
-    def get(self):
+    def get(self, agent):
         args = request.args
         credentials = args['credentials']
-        agent = args['agent']
         api_key = args['api_key']
 
-        return {'hello': 'stuff'}
+        return {'hello': id}
 
 
-api.add_resource(Balance, '/balance')
+api.add_resource(Balance, '/<string:agent>/balance/', endpoint="api.points_balance")
+
+
+class Init(Resource):
+
+    def get(self):
+        agents = []
+        # Not all services will provide points and transactions
+        # TODO: we should detect this dynamically
+        for agent in active.AGENTS:
+            agents.append({
+                'name': agent[0],
+                'services': {
+                    'points': url_for('api.points_balance', id=agent[0]),
+                    'transactions': '',
+                    'pointsAndTransactions': ''
+                }
+            })
+
+        response_data = {'agents': agents}
+        return response_data
+
+api.add_resource(Init, '/')
+
