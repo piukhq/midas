5d22a4f0b3fa2bed59b4b1144a59b0d37039f466
Chris Latham <cl@loyaltyangels.com>
2015-10-20 17:41:10 +0100

    add eurostar agent and test

---
+++ app/agents/eurostar.py
@@ -0,0 +1,29 @@
+from app.agents.base import Miner
+from app.agents.exceptions import STATUS_LOGIN_FAILED
+from app.utils import extract_decimal
+from decimal import Decimal
+import arrow
+
+
+class Eurostar(Miner):
+    def login(self, credentials):
+        self.open_url('https://www.eurostar.com/uk-en/login')
+
+        login_form = self.browser.get_form('user-account-management-login-or-create-account-form')
+        login_form['name'].value = credentials['email']
+        login_form['pass'].value = credentials['password']
+
+        self.browser.submit_form(login_form)
+
+        selector = 'div.messages.error > ul > li'
+        self.check_error('/uk-en/login', ((selector, STATUS_LOGIN_FAILED, 'Sorry'),))
+
+    def balance(self):
+        self.open_url('https://www.eurostar.com/uk-en/account')
+        point_holder = self.browser.select('div.pane-you-are-epp-member-markup div.pane-content div.row div.right')
+        return {
+            'points': extract_decimal(point_holder.text)
+        }
+
+    def transactions(self):
+        return None
---
+++ app/tests/service/test_eurostar.py
@@ -0,0 +1,30 @@
+import unittest
+from app.agents.exceptions import LoginError
+from app.agents.eurostar import Eurostar
+from app.agents import schemas
+from app.tests.service.logins import CREDENTIALS
+
+
+class TestEurostar(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.e = Eurostar(1, 1)
+        cls.e.attempt_login(CREDENTIALS['eurostar'])
+
+    def test_login(self):
+        self.assertEqual(self.e.browser.response.status_code, 200)
+
+    def test_balance(self):
+        balance = self.e.balance()
+        schemas.balance(balance)
+
+
+class TestEurostarFail(unittest.TestCase):
+    def test_login_fail(self):
+        eu = Eurostar(1, 1)
+        with self.assertRaises(LoginError) as e:
+            eu.attempt_login(CREDENTIALS['bad'])
+        self.assertEqual(e.exception.name, 'STATUS_LOGIN_FAILED')
+
+if __name__ == '__main__':
+    unittest.main()
