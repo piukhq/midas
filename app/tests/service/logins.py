bf8e7375148ec0e561d6016e267c880a4f75f316
Chris Latham <cl@loyaltyangels.com>
2015-10-22 10:56:17 +0100

    put comma back on esprit password entry

--- app/tests/service/logins.py
+++ app/tests/service/logins.py
@@ -76,7 +76,7 @@ CREDENTIALS = {
     },
     "esprit": {
         "email": "chris.gormley2@me.com",
-        "password": "SDHansbrics6"
+        "password": "SDHansbrics6",
     },
     "quidco": {
         "email": "chris.smith4@gmx.co.uk",
