5b8695c42672bd18cdec65871dee898c9d7ab028
Chris Latham <cl@loyaltyangels.com>
2015-10-23 09:53:12 +0100

    fix eurostar agent login

--- app/agents/eurostar.py
+++ app/agents/eurostar.py
@@ -5,20 +5,12 @@ from app.utils import extract_decimal
 
 class Eurostar(Miner):
     def login(self, credentials):
-        """self.open_url('https://www.eurostar.com/uk-en/login')
-
-        login_form = self.browser.get_form('user-account-management-login-or-create-account-form')
-        login_form['name'].value = credentials['email']
-        login_form['pass'].value = credentials['password']
-
-        self.browser.submit_form(login_form)"""
-
         data = {
             'form_build_id': 'form-e_ry4AzklRERsn0JlnLv6a7YeffGMmj9Llrgbb2ozaM',
             'form_id': 'user_account_management_login_or_create_account_form',
-            'name': 'chris.gormley2@me.com',
+            'name': credentials['email'],
             'login_or_create': 'login',
-            'pass': 'QDHansbrics8',
+            'pass': credentials['password'],
             'login_form_submit': 'Continue',
         }
         self.browser.open('https://www.eurostar.com/uk-en/login', method='post', data=data)
@@ -27,8 +19,7 @@ class Eurostar(Miner):
         self.check_error('/uk-en/login', ((selector, STATUS_LOGIN_FAILED, 'Sorry'),))
 
     def balance(self):
-        self.open_url('https://www.eurostar.com/uk-en/account')
-        point_holder = self.browser.select('div.pane-you-are-epp-member-markup div.pane-content div.row div.right')
+        point_holder = self.browser.select('div.pane-you-are-epp-member-markup div.pane-content div.row div.right')[1]
         return {
             'points': extract_decimal(point_holder.text)
         }
