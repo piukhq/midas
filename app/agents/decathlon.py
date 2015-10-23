3ec0a60dd5d0203209530b79c2bc1d9b76fc563a
Chris Latham <cl@loyaltyangels.com>
2015-10-23 12:44:56 +0100

    add decathlon login and points scraping

--- app/agents/decathlon.py
+++ app/agents/decathlon.py
@@ -1,28 +1,27 @@
 from app.agents.base import Miner
-from app.agents.exceptions import STATUS_LOGIN_FAILED
+from app.agents.exceptions import STATUS_LOGIN_FAILED, LoginError
 from app.utils import extract_decimal
 
 
 class Decathlon(Miner):
     def login(self, credentials):
-        #self.open_url('https://www.decathlon.co.uk/en/loginPage')
+        url = ('https://www.decathlon.co.uk/en/loginAjax'
+               '?USERNAME={}&PASSWORD={}').format(credentials['email'], credentials['password'])
 
-        selector = ''
-        self.check_error('', ((selector, STATUS_LOGIN_FAILED, ''),))
+        self.open_url(url)
+
+        result = self.browser.select('p')[0].text.strip()
+        if result != 'Connected':
+            raise LoginError(STATUS_LOGIN_FAILED)
 
     def balance(self):
-        self.open_url('')
+        self.open_url('https://www.decathlon.co.uk/en/mydktAvantages'
+                      '?currentMenu=MenuMyAccountWebLinkAvantages#nomPage=defaut')
 
-        point_holder = self.browser.select('')[0]
+        point_holder = self.browser.select('#menu-my-account-infos-loyalty-card-point span.loyalty-content')[0]
         return {
             'points': extract_decimal(point_holder.text)
         }
 
-    @staticmethod
-    def parse_transaction(row):
-        raise NotImplementedError()
-
     def transactions(self):
-        self.open_url('')
-        rows = self.browser.select('')
-        return [self.hashed_transaction(row) for row in rows]
+        return None
--- app/tests/service/test_decathlon.py
+++ app/tests/service/test_decathlon.py
@@ -8,16 +8,11 @@ from app.tests.service.logins import CREDENTIALS
 class TestDecathlon(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.d = Decathlon(1, 1)
+        cls.d = Decathlon(1, 1, False)
         cls.d.attempt_login(CREDENTIALS['decathlon'])
 
     def test_login(self):
         self.assertEqual(self.d.browser.response.status_code, 200)
-
-    def test_transactions(self):
-        transactions = self.d.transactions()
-        self.assertTrue(transactions)
-        schemas.transactions(transactions)
 
     def test_balance(self):
         balance = self.d.balance()
@@ -26,7 +21,7 @@ class TestDecathlon(unittest.TestCase):
 
 class TestDecathlonFail(unittest.TestCase):
     def test_login_fail(self):
-        d = Decathlon(1, 1)
+        d = Decathlon(1, 1, False)
         with self.assertRaises(LoginError) as e:
             d.attempt_login(CREDENTIALS['bad'])
         self.assertEqual(e.exception.name, 'STATUS_LOGIN_FAILED')
