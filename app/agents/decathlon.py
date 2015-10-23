d03ce69354501a81b2cb1b6f651bb6cfe3cec622
Chris Latham <cl@loyaltyangels.com>
2015-10-23 14:55:28 +0100

    move token request to transaction method

--- app/agents/decathlon.py
+++ app/agents/decathlon.py
@@ -21,17 +21,6 @@ class Decathlon(Miner):
         if result != 'Connected':
             raise LoginError(STATUS_LOGIN_FAILED)
 
-        # Obtain the account token.
-        self.open_url('https://back.mydecathlon.com/mydkt-server-mvc/ajax/private/authentification/connexionCode'
-                      '?ppays=GB&codeAppli=Osmose&response_type=code&client_id=osmose_ecuk&langue=en'
-                      '&host=www.decathlon.co.uk&resterConnecter=true'
-                      '&mdp=tBlZFD4vFf9iF1x4&email=O1bwQNsqP19wRMvfUJtYRMKqO19p'
-                      '&__preventCache__=1445596341121&callback=__gwt_jsonp__.P1.onSuccess')
-
-        response_text = self.json_pattern.findall(self.browser.response.text)[0]
-        data = json.loads(response_text)
-        self.token = data['data']['token']
-
     def balance(self):
         self.open_url('https://www.decathlon.co.uk/en/mydktAvantages'
                       '?currentMenu=MenuMyAccountWebLinkAvantages#nomPage=defaut')
@@ -46,16 +35,26 @@ class Decathlon(Miner):
         return row
 
     def transactions(self):
+        # Obtain the account token.
+        self.open_url('https://back.mydecathlon.com/mydkt-server-mvc/ajax/private/authentification/connexionCode'
+                      '?ppays=GB&codeAppli=Osmose&response_type=code&client_id=osmose_ecuk&langue=en'
+                      '&host=www.decathlon.co.uk&resterConnecter=true'
+                      '&mdp=tBlZFD4vFf9iF1x4&email=O1bwQNsqP19wRMvfUJtYRMKqO19p'
+                      '&__preventCache__=1445596341121&callback=__gwt_jsonp__.P1.onSuccess')
+
+        token_response = json.loads(self.json_pattern.findall(self.browser.response.text)[0])
+        token = token_response['data']['token']
+
         self.open_url('http://back.mydecathlon.com/mydkt-server-mvc/ajax/private/tickets/liste'
                       '?ppays=GB&isStoreReceipt=t&isTransfert=false&esdljkdl={}'
-                      '&__preventCache__=1445600047005&callback=__gwt_jsonp__.P9.onSuccess'.format(self.token))
+                      '&__preventCache__=1445600047005&callback=__gwt_jsonp__.P9.onSuccess'.format(token))
         first_response = json.loads(self.json_pattern.findall(self.browser.response.text)[0])
 
         self.open_url('http://back.mydecathlon.com/mydkt-server-mvc/ajax/private/tickets/listeDetail'
                       '?ppays=GB&isStoreReceipt=t&isTransfert=false'
                       '&entetes=%5B%7B%22typeTiers%22%3A7%2C+%22numTiers%22%3A718%2C+%22sousNumTiers'
                       '%22%3A718%2C+%22tetId%22%3A404543%7D%5D&esdljkdl={}&__preventCache__=1445600047072'
-                      '&callback=__gwt_jsonp__.P10.onSuccess'.format(self.token))
+                      '&callback=__gwt_jsonp__.P10.onSuccess'.format(token))
         second_response = json.loads(self.json_pattern.findall(self.browser.response.text)[0])
 
         rows = [{
