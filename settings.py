58bec82a6b64a526a3cda2f6c59411e756f9d3bb
Andrew Kenyon <ak@loyaltyangels.com>
2015-10-27 16:28:44 +0000

    added sentry support

--- settings.py
+++ settings.py
@@ -18,3 +18,6 @@ HADES_URL = env_var("HADES_URL", "http://local.hades.chingrewards.com:8000")
 HERMES_URL = env_var("HERMES_URL", "http://local.hermes.chingrewards.com:8000")
 
 SERVICE_API_KEY = 'F616CE5C88744DD52DB628FAD8B3D'
+
+SENTRY_DNS = env_var("MIDAS_SENTRY_DNS",
+                     "http://8dac759f053a44f685d6d2a1227910b1:7d603ac97f664536a0a568a28de06f86@192.168.1.53:8999/3")
