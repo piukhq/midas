ea9430cedd81ff6514b99f32774bf6c37573aebc
Chris Latham <cl@loyaltyangels.com>
2015-10-28 09:19:22 +0000

    add harrods agent

---
+++ app/tests/service/test_harrods.py
@@ -0,0 +1,35 @@
+import unittest
+from app.agents.harrods import Harrods
+from app.agents.exceptions import LoginError
+from app.tests.service.logins import CREDENTIALS
+from app.agents import schemas
+
+
+class TestHarrods(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.h = Harrods(1, 1)
+        cls.h.attempt_login(CREDENTIALS['harrods'])
+
+    def test_login(self):
+        self.assertEqual(self.h.browser.url, 'https://www.harrods.com/Pages/Account/Secure/AccountHome.aspx')
+
+    def test_balance(self):
+        b = self.h.balance()
+        schemas.balance(b)
+
+    def test_transactions(self):
+        t = self.h.transactions()
+        self.assertTrue(t)
+        schemas.transactions(t)
+
+
+class TestHarrodsFail(unittest.TestCase):
+    def test_bad_login(self):
+        h = Harrods(1, 1)
+        with self.assertRaises(LoginError) as e:
+            h.attempt_login(CREDENTIALS['bad'])
+        self.assertEqual(e.exception.name, 'STATUS_LOGIN_FAILED')
+
+if __name__ == '__main__':
+    unittest.main()
