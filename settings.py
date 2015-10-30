b67f8119c7b0c941f55394e2fa9f6bf9aa0bf2cc
Ben Olsen <bo@loyaltyangels.com>
2015-10-30 10:50:40 +0000

    add local setting

--- settings.py
+++ settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'QlLWJYCugcMQ59nIWh5lnHBMcgHtLupJrv4SvohR'
 APP_DIR = os.path.abspath(os.path.dirname(__file__))
 PROJECT_ROOT = os.path.abspath(os.path.join(APP_DIR, os.pardir))
 DEBUG = env_var("MIDAS_DEBUG", False)
-
+LOCAL = env_var("MIDAS_LOCAL", False)
 AES_KEY = "6gZW4ARFINh4DR1uIzn12l7Mh1UF982L"
 
 REDIS_URL = env_var("MIDAS_REDIS_URI", "redis://localhost:6379/0")
