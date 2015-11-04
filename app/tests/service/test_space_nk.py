f7c1770b8ac722fecb6940481c17a8e89eca38e9
Ben Olsen <bo@loyaltyangels.com>
2015-11-04 12:09:10 +0000

    handle login error exception

--- app/tests/service/test_space_nk.py
+++ app/tests/service/test_space_nk.py
@@ -28,7 +28,7 @@ class TestSpaceNKFail(unittest.TestCase):
     def test_login_fail(self):
         m = SpaceNK(1, 1)
         with self.assertRaises(LoginError) as e:
-            m.attempt_login({ 'card_number': '99999999999999999999' })
+            m.attempt_login({'card_number': '99999999999999999999' })
         self.assertEqual(e.exception.name, 'STATUS_LOGIN_FAILED')
 
 if __name__ == '__main__':
