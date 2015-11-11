5869849efcdbee0189528fcc1b95deff7744973d
Chris Latham <cl@loyaltyangels.com>
2015-11-11 11:56:27 +0000

    lufthansa agent balance schema and login form fix

--- app/agents/lufthansa.py
+++ app/agents/lufthansa.py
@@ -1,17 +1,31 @@
 from app.agents.base import Miner
 from app.agents.exceptions import LoginError, STATUS_LOGIN_FAILED, STATUS_ACCOUNT_LOCKED
 from app.utils import extract_decimal
+from decimal import Decimal
 import arrow
 
 
 class Lufthansa(Miner):
     def login(self, credentials):
-        self.open_url('https://www.miles-and-more.com/online/myportal/mam/uk/account/account_statement?l=en'
-                      '&cid=1000243&WT.mc_id=MmhpabMpar0314arwal')
+        self.open_url('https://www.miles-and-more.com/online/myportal/mam/uk/account/account_statement')
 
-        login_form = self.browser.get_form('PC_7_A0SO5GGCQUEA20A15AJ3TA3QK20n3046_mam-usm-cardnr-form')
-        login_form['PC_7_A0SO5GGCQUEA20A15AJ3TA3QK20n3046_userId'].value = credentials['card_number']
-        login_form['PC_7_A0SO5GGCQUEA20A15AJ3TA3QK20n3046_password'].value = credentials['pin']
+        # Both the form's ID and its field names are partially scrambled.
+        login_form = [x for x in self.browser.get_forms()
+                      if x.action.startswith('https://www.miles-and-more.com/online/portal/mam/uk/profilelogin')][0]
+
+        card_number_done = False
+        pin_done = False
+        for field in login_form.fields:
+            if field.endswith('userId'):
+                login_form[field] = credentials['card_number']
+                card_number_done = True
+            elif field.endswith('password'):
+                login_form[field] = credentials['pin']
+                pin_done = True
+
+            # Don't waste time searching for more inputs if we've already found them both.
+            if card_number_done and pin_done:
+                break
 
         self.browser.submit_form(login_form)
 
@@ -26,7 +40,9 @@ class Lufthansa(Miner):
         award_miles = extract_decimal(miles_elements[0].text)
         status_miles = extract_decimal(miles_elements[1].text)
         return {
-            'points': award_miles + status_miles
+            'points': award_miles + status_miles,
+            'value': Decimal('0'),
+            'value_label': '{} miles'.format(award_miles + status_miles),
         }
 
     @staticmethod
