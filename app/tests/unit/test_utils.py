59cb1998f230485c3d27707e227bca64ae7fae91
Chris Latham <cl@loyaltyangels.com>
2015-12-03 10:08:08 +0000

    change extract_decimal to support comma-grouped number formats

--- app/tests/unit/test_utils.py
+++ app/tests/unit/test_utils.py
@@ -9,6 +9,7 @@ class TestUtils(unittest.TestCase):
         self.assertEqual(extract_decimal("sdfg 23.33 dfg"), Decimal("23.33"))
         self.assertEqual(extract_decimal("233"), Decimal("233"))
         self.assertEqual(extract_decimal("23.1 hhh"), Decimal("23.1"))
+        self.assertEqual(extract_decimal("24,412.51"), Decimal("24412.51"))
 
     def test_generate_random_key(self):
         self.assertEqual(type(generate_random_key(1)), bytes)
