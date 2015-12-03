0bd9ad323b35767a64fa98019baf8705f9de796f
Chris Latham <cl@loyaltyangels.com>
2015-12-03 10:09:59 +0000

    allow any number of digits in each comma group

--- app/utils.py
+++ app/utils.py
@@ -13,7 +13,7 @@ def extract_decimal(s):
     We need to use the quantize method to ensure whole
     numbers do not become integers when json encoding
     """
-    return Decimal(re.search(r'-?\d+(?:,\d{1,3})*(?:\.\d+)?', s).group(0).replace(',', '')).quantize(TWO_PLACES)
+    return Decimal(re.search(r'-?\d+(?:,\d+)*(?:\.\d+)?', s).group(0).replace(',', '')).quantize(TWO_PLACES)
 
 
 def open_browser(html, base_href):
