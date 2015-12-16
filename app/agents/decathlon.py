523db218206dbac6ca74db1cd7101e2424e2f5ad
Chris Latham <cl@loyaltyangels.com>
2015-12-16 09:49:48 +0000

    fix decathlon login

--- app/agents/decathlon.py
+++ app/agents/decathlon.py
@@ -2,7 +2,10 @@ from app.agents.base import Miner
 from app.agents.exceptions import STATUS_LOGIN_FAILED, LoginError
 from app.utils import extract_decimal
 from decimal import Decimal, ROUND_DOWN
+import execjs
+import json
 import re
+import os
 
 
 class Decathlon(Miner):
@@ -10,20 +13,40 @@ class Decathlon(Miner):
     point_conversion_rate = Decimal('0.004')
 
     def login(self, credentials):
-        self.open_url('https://www.decathlon.co.uk/en/loginAjax'
-                      '?USERNAME={}&PASSWORD={}'.format(credentials['email'], credentials['password']))
+        script_dir = os.path.dirname(__file__)
+        rel_path = 'js/decathlon-decode.js'
+        decoder_path = os.path.join(script_dir, rel_path)
+        with open(decoder_path) as decode_js:
+            js = decode_js.read()
+            decoder = execjs.compile(js)
 
-        result = self.browser.select('p')[0].text.strip()
-        if result != 'Connected':
-            raise LoginError(STATUS_LOGIN_FAILED)
+        result = decoder.call('hash_credentials', credentials['email'], credentials['password'])
+
+        mdp = result['mdp']
+        email = result['email']
+        url = (
+            'https://back.mydecathlon.com/mydkt-server-mvc/ajax'
+            '/private/authentification/connexionCode'
+            '?ppays=GB'
+            '&codeAppli=Osmose'
+            '&response_type=code'
+            '&client_id=osmose_ecuk'
+            '&langue=en'
+            '&host=www.decathlon.co.uk'
+            '&resterConnecter=true'
+            '&mdp={}'
+            '&email={}'
+            '&__preventCache__=1450195310293'
+            '&callback='
+        ).format(mdp, email)
+
+        self.open_url(url)
+        resp = json.loads(self.browser.response.text[1:-1])
 
     def balance(self):
-        self.open_url('https://www.decathlon.co.uk/en/mydktAvantages'
-                      '?currentMenu=MenuMyAccountWebLinkAvantages#nomPage=defaut')
-
-        points = extract_decimal(
-            self.browser.select('#menu-my-account-infos-loyalty-card-point span.loyalty-content')[0].text)
-
+        self.open_url('https://www.decathlon.co.uk/en/viewprofile')
+        points = extract_decimal(self.browser.select
+                                 ('#menu-my-account-infos-loyalty-card-point > span.loyalty-content')[0].text.strip())
         value = self.calculate_point_value(points).quantize(0, ROUND_DOWN)
 
         return {
