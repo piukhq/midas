57c241908d3323735610ec04daee6e36a3fe6f09
Chris Latham <cl@loyaltyangels.com>
2015-12-17 09:52:09 +0000

    add decathlon transactions and error handling

--- app/agents/decathlon.py
+++ app/agents/decathlon.py
@@ -3,13 +3,12 @@ from app.agents.exceptions import STATUS_LOGIN_FAILED, LoginError
 from app.utils import extract_decimal
 from decimal import Decimal, ROUND_DOWN
 import execjs
+import arrow
 import json
-import re
 import os
 
 
 class Decathlon(Miner):
-    json_pattern = re.compile('__gwt_jsonp__\.P\d+\.onSuccess\((.*)\)')
     point_conversion_rate = Decimal('0.004')
 
     def login(self, credentials):
@@ -24,29 +23,36 @@ class Decathlon(Miner):
 
         mdp = result['mdp']
         email = result['email']
-        url = (
-            'https://back.mydecathlon.com/mydkt-server-mvc/ajax'
-            '/private/authentification/connexionCode'
-            '?ppays=GB'
-            '&codeAppli=Osmose'
-            '&response_type=code'
-            '&client_id=osmose_ecuk'
-            '&langue=en'
-            '&host=www.decathlon.co.uk'
-            '&resterConnecter=true'
-            '&mdp={}'
-            '&email={}'
-            '&__preventCache__=1450195310293'
-            '&callback='
-        ).format(mdp, email)
+        url = ('http://back.mydecathlon.com/mydkt-server-mvc/ajax'
+               '/private/authentification/connexion'
+               '?ppays=GB'
+               '&codeAppli=NetCardV2'
+               '&langue=EN'
+               '&resterConnecter=true'
+               '&mdp={}'
+               '&email={}'
+               '&callback='
+               ).format(mdp, email)
 
         self.open_url(url)
         resp = json.loads(self.browser.response.text[1:-1])
 
+        if resp['data']['codeRetour'] == '404':
+            raise LoginError(STATUS_LOGIN_FAILED)
+
+        self.token = resp['data']['token']
+
     def balance(self):
-        self.open_url('https://www.decathlon.co.uk/en/viewprofile')
-        points = extract_decimal(self.browser.select
-                                 ('#menu-my-account-infos-loyalty-card-point > span.loyalty-content')[0].text.strip())
+        self.open_url('http://back.mydecathlon.com/mydkt-server-mvc/ajax'
+                      '/private/synthesePersonne/getInfoPersonne'
+                      '?ppays=GB'
+                      '&personneId=0'
+                      '&esdljkdl={}'
+                      '&callback='.format(self.token))
+
+        resp = json.loads(self.browser.response.text[1:-1])
+        points = Decimal(resp['data']['soldePoint']['solde'])
+
         value = self.calculate_point_value(points).quantize(0, ROUND_DOWN)
 
         return {
@@ -55,5 +61,50 @@ class Decathlon(Miner):
             'value_label': self.format_label(value, '£5 voucher'),
         }
 
+    @staticmethod
+    def parse_transaction(row):
+        return {
+            'date': arrow.get(row['date']),
+            'description': row['description'],
+            'points': extract_decimal(row['points']),
+            'value': extract_decimal(row['value']),
+            'location': row['location'],
+        }
+
     def scrape_transactions(self):
-        return None
+        self.open_url('http://back.mydecathlon.com/mydkt-server-mvc/ajax/private'
+                      '/tickets/liste'
+                      '?ppays=GB'
+                      '&idPersonne=0'
+                      '&isStoreReceipt=t'
+                      '&isTransfert=false'
+                      '&esdljkdl={}'
+                      '&callback='.format(self.token))
+
+        list = json.loads(self.browser.response.text[1:-1])
+
+        self.open_url('http://back.mydecathlon.com/mydkt-server-mvc/ajax/private'
+                      '/tickets/listeDetail'
+                      '?ppays=GB'
+                      '&idPersonne=0'
+                      '&isStoreReceipt=t'
+                      '&isTransfert=false'
+                      '&entetes=%5B%7B%22typeTiers%22%3A7%2C+%22sousNumTiers%22%3A718%2C'
+                      '+%22numTiers%22%3A718%2C+%22tetId%22%3A404543%7D%5D'
+                      '&esdljkdl={}'
+                      '&callback='.format(self.token))
+
+        detail = json.loads(self.browser.response.text[1:-1])
+
+        transactions = []
+        for i, t in enumerate(list['data']):
+            d = detail['data'][i]
+            transactions.append({
+                'date': t['dateTicket'],
+                'description': 'Transaction #{}'.format(t['tetId']),
+                'points': str(d['credits'][0]['nombreDePoints']),
+                'value': str(d['montantTicket']),
+                'location': t['nomTiers'],
+            })
+
+        return transactions
