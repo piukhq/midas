84e77ac034015f3e8d216165f12c25b4b2dc5746
Chris Latham <cl@loyaltyangels.com>
2015-12-18 16:42:47 +0000

    add END_SITE_DOWN for hyatt site

--- app/agents/hyatt.py
+++ app/agents/hyatt.py
@@ -1,5 +1,5 @@
 from app.agents.base import Miner
-from app.agents.exceptions import LoginError, STATUS_LOGIN_FAILED
+from app.agents.exceptions import LoginError, STATUS_LOGIN_FAILED, END_SITE_DOWN
 from app.utils import extract_decimal
 from decimal import Decimal
 
@@ -7,6 +7,11 @@ from decimal import Decimal
 class Hyatt(Miner):
     def login(self, credentials):
         self.open_url('https://www.hyatt.com/gp/en/index.jsp')
+
+        error_message = self.browser.select('#hyattBuilt_pgctntClmnQuickBookAndSearchModuleHolder > div > div > p')
+        mtce_text = 'The Hyatt Gold Passport system is undergoing maintenance at this time.'
+        if error_message and error_message[0].text.strip() == mtce_text:
+            raise LoginError(END_SITE_DOWN)
 
         login_form = self.browser.get_form('signin')
         login_form['username'].value = credentials['username']
