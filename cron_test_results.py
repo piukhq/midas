44a58548fef6445257531070797c5f7459951df2
Chris Latham <cl@loyaltyangels.com>
2016-02-10 11:22:39 +0000

    post to #errors-agents instead of test chat

--- cron_test_results.py
+++ cron_test_results.py
@@ -107,5 +107,4 @@ if __name__ == '__main__':
         with open(JUNIT_XML_FILENAME) as f:
             test_results = xmltodict.parse(f.read())
         message = generate_message(test_results)
-        #Slacker(SLACK_API_KEY).chat.post_message('#errors-agents', message)
-        Slacker(SLACK_API_KEY).chat.post_message('@chris', message)
+        Slacker(SLACK_API_KEY).chat.post_message('#errors-agents', message)
