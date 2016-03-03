f5d148894b0ef1045a4b49a3653a4e495f69e036
Chris Latham <cl@loyaltyangels.com>
2016-03-03 10:27:24 +0000

    Revert "Merge branch 'feature/error-table' into 'master'"
    This reverts commit ea3b890bb193b321861d7a7706bf32cd7146b545, reversing
    changes made to 98a6368ab9cb6bccab7a3897d3fa725a766e93c6.

--- app/tests/unit/test_cron_results.py
+++ app/tests/unit/test_cron_results.py
@@ -2,8 +2,7 @@ import unittest
 import xmltodict
 import os
 from os.path import join
-from cron_test_results import generate_message, format_table, parse_test_results
-from collections import defaultdict
+from cron_test_results import generate_message
 
 
 class TestCronResults(unittest.TestCase):
@@ -15,20 +14,3 @@ class TestCronResults(unittest.TestCase):
         self.assertIn('Nandos', message)
         self.assertIn('Starbucks', message)
         self.assertIn('*End site down:*', message)
-
-    def test_parse_test_results(self):
-        with open(join(os.path.dirname(os.path.abspath(__file__)), 'fixtures/example_test_results.xml')) as f:
-            test_results = xmltodict.parse(f.read())
-        failures = parse_test_results(test_results)
-        self.assertIsInstance(failures, defaultdict)
-        self.assertIn('nandos', failures)
-        self.assertEqual(len(failures['nandos']), 4)
-        self.assertEqual(failures['nandos'][0], 'test balance')
-
-    def test_format_table(self):
-        with open(join(os.path.dirname(os.path.abspath(__file__)), 'fixtures/example_test_results.xml')) as f:
-            test_results = xmltodict.parse(f.read())
-        failures = parse_test_results(test_results)
-        table = format_table(failures)
-        self.assertIn('Nandos', table)
-        self.assertIn('Starbucks', table)
