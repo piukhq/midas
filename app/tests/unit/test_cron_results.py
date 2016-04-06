6328e81007ded6f784162efbce40022c9d9e4674
Chris Latham <cl@loyaltyangels.com>
2016-04-06 17:12:21 +0100

    remove uneeded tests

--- app/tests/unit/test_cron_results.py
+++ app/tests/unit/test_cron_results.py
@@ -2,20 +2,11 @@ import unittest
 import xmltodict
 import os
 from os.path import join
-from cron_test_results import generate_message, format_table, parse_test_results
+from cron_test_results import parse_test_results
 from collections import defaultdict
 
 
 class TestCronResults(unittest.TestCase):
-    def test_generate_message(self):
-        with open(join(os.path.dirname(os.path.abspath(__file__)), 'fixtures/example_test_results.xml')) as f:
-            test_results = xmltodict.parse(f.read())
-        message = generate_message(test_results)
-        self.assertTrue(message.startswith("*Total errors:* 17"))
-        self.assertIn('Nandos', message)
-        self.assertIn('Starbucks', message)
-        self.assertIn('*End site down:*', message)
-
     def test_parse_test_results(self):
         with open(join(os.path.dirname(os.path.abspath(__file__)), 'fixtures/example_test_results.xml')) as f:
             test_results = xmltodict.parse(f.read())
@@ -24,11 +15,3 @@ class TestCronResults(unittest.TestCase):
         self.assertIn('nandos', failures)
         self.assertEqual(len(failures['nandos']), 4)
         self.assertEqual(failures['nandos'][0], 'test balance')
-
-    def test_format_table(self):
-        with open(join(os.path.dirname(os.path.abspath(__file__)), 'fixtures/example_test_results.xml')) as f:
-            test_results = xmltodict.parse(f.read())
-        failures = parse_test_results(test_results)
-        table = format_table(failures)
-        self.assertIn('Nandos', table)
-        self.assertIn('Starbucks', table)
