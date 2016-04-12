6de4433135f94348e3b65dc07f7e6a1e79b4d4b3
Chris Latham <cl@loyaltyangels.com>
2016-04-12 15:11:36 +0100

    fix maximiles agent login code

--- app/agents/maximiles.py
+++ app/agents/maximiles.py
@@ -9,14 +9,29 @@ class Maximiles(Miner):
     point_conversion_rate = Decimal('0.0009')
 
     def login(self, credentials):
-        url = 'https://www.maximiles.co.uk/my-account/login'
-        self.open_url(url)
+        self.open_url('http://www.maximiles.co.uk/my-account/login')
+
+        self.headers = {
+            'Host': 'www.maximiles.co.uk',
+            'Connection': 'keep-alive',
+            'Content-Length': '140',
+            'Cache-Control': 'max-age=0',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+            'Origin': 'http://www.maximiles.co.uk',
+            'Upgrade-Insecure-Requests': '1',
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.75 '
+                          'Safari/537.36',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Referer': 'http://www.maximiles.co.uk/my-account/login',
+            'Accept-Encoding': 'gzip, deflate',
+            'Accept-Language': 'en-GB,en-US;q=0.8,en;q=0.6',
+        }
 
         login_form = self.browser.get_form('infos')
         login_form['maximiles_id'].value = credentials['email']
         login_form['passe'].value = credentials['password']
 
-        self.browser.submit_form(login_form)
+        self.browser.submit_form(login_form, headers=self.headers)
 
         selector = 'div.error > ul > li'
         self.check_error('/my-account/login', ((selector, STATUS_LOGIN_FAILED, 'Invalid Username/password'),))
