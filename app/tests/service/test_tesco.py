a272444f9a059fcbadcfa0126da87353c75cdc88
Chris Latham <cl@loyaltyangels.com>
2016-05-09 09:52:36 +0100

    fix tesco agent test

--- app/tests/service/test_tesco.py
+++ app/tests/service/test_tesco.py
@@ -29,7 +29,7 @@ class TestTesco(unittest.TestCase):
 
     def test_transactions(self):
         transactions = self.b.transactions()
-        self.assertTrue(transactions)
+        self.assertIsNotNone(transactions)
         schemas.transactions(transactions)
 
     def test_balance(self):
