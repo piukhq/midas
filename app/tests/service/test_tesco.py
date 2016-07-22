03ca03fd8293e54182f40ff4ad6d5fa6667927f3
Chris Latham <cl@loyaltyangels.com>
2016-07-22 14:29:04 +0100

    User Story #2004 update tesco agent to work with new end-site

--- app/tests/service/test_tesco.py
+++ app/tests/service/test_tesco.py
@@ -14,7 +14,7 @@ class TestTesco(unittest.TestCase):
 
     def test_login(self):
         self.assertEqual(self.b.browser.response.status_code, 200)
-        self.assertEqual(urlsplit(self.b.browser.url).path, '/Clubcard/MyAccount/Home.aspx')
+        self.assertEqual(urlsplit(self.b.browser.url).path, '/Clubcard/MyAccount/Alpha443/home/Home')
 
     def test_login_with_card_number(self):
         b = Tesco(1, 1)
@@ -25,7 +25,7 @@ class TestTesco(unittest.TestCase):
         }
         b.attempt_login(credentials)
         self.assertEqual(b.browser.response.status_code, 200)
-        self.assertEqual(urlsplit(b.browser.url).path, '/Clubcard/MyAccount/Home.aspx')
+        self.assertEqual(urlsplit(b.browser.url).path, '/Clubcard/MyAccount/Alpha443/home/Home')
 
     def test_transactions(self):
         transactions = self.b.transactions()
