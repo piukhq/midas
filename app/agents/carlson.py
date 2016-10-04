9332482f90437cd29f8794c757a6b10ae78af914
Nico Scordialo <nicoscordialo@googlemail.com>
2016-10-04 13:32:57 +0100

    changed error message for carlson

--- app/agents/carlson.py
+++ app/agents/carlson.py
@@ -7,56 +7,56 @@ import arrow
 
 class Carlson(Miner):
     def login(self, credentials):
-        url = 'https://www.clubcarlson.com/secure/login.do'
+        url = "https://www.clubcarlson.com/secure/login.do"
         data = {
-            'accountEmail': '',
-            'linkToken': '',
-            'successURL': '/home.do',
-            'rightSide': '',
-            'userId': credentials['username'],
-            'password': credentials['password'],
-            'rememberMe': 'true',
+            "accountEmail": "",
+            "linkToken": "",
+            "successURL": "https://www.clubcarlson.com/myaccount/secure/?login=true",
+            "rightSide": "",
+            "userId": credentials["username"],
+            "password": credentials["password"],
+            "rememberMe": "true",
         }
-        self.open_url(url, method='post', data=data)
+        self.open_url(url, method="post", data=data)
 
-        self.check_error('/secure/login.do',
-                         (('div.globalerrors', STATUS_LOGIN_FAILED, 'Your sign in was unsuccessful.'), ))
+        self.check_error("/secure/login.do",
+                         (("div.globalerrors", STATUS_LOGIN_FAILED, "Your email or password"), ))
 
     def balance(self):
-        points = extract_decimal(self.browser.select('span.goldpointsBalance')[0].text)
+        points = extract_decimal(self.browser.select("span.goldpointsBalance")[0].text)
 
         reward = self.calculate_tiered_reward(points, [
-            (105000, 'Premium award night, category 7'),
-            (45000, 'Premium award night, category 6'),
-            (70000, 'Standard award night, category 7'),
-            (66000, 'Premium award night, category 5'),
-            (50000, 'Standard award night, category 6'),
-            (44000, 'Standard award night, category 5'),
-            (42000, 'Premium award night, category 3'),
-            (38000, 'Standard award night, category 4'),
-            (24000, 'Standard award night, category 3'),
-            (22500, 'Premium award night, category 2'),
-            (15000, 'Standard award night, category 2'),
-            (13500, 'Premium award night, category 1'),
-            (9000, 'Standard award night, category 1'),
+            (105000, "Premium award night, category 7"),
+            (45000, "Premium award night, category 6"),
+            (70000, "Standard award night, category 7"),
+            (66000, "Premium award night, category 5"),
+            (50000, "Standard award night, category 6"),
+            (44000, "Standard award night, category 5"),
+            (42000, "Premium award night, category 3"),
+            (38000, "Standard award night, category 4"),
+            (24000, "Standard award night, category 3"),
+            (22500, "Premium award night, category 2"),
+            (15000, "Standard award night, category 2"),
+            (13500, "Premium award night, category 1"),
+            (9000, "Standard award night, category 1"),
         ])
 
         return {
-            'points': points,
-            'value': Decimal('0'),
-            'value_label': reward,
+            "points": points,
+            "value": Decimal("0"),
+            "value_label": reward,
         }
 
-    # TODO: Parse transactions. Not done yet because there's no transaction data in the account.
+    # TODO: Parse transactions. Not done yet because there"s no transaction data in the account.
     @staticmethod
     def parse_transaction(row):
         return row
 
     def scrape_transactions(self):
-        # self.open_url('https://www.clubcarlson.com/myaccount/secure/loyalty/transactionHistory.do')
+        # self.open_url("https://www.clubcarlson.com/myaccount/secure/loyalty/transactionHistory.do")
         t = {
-            'date': arrow.get(0),
-            'description': 'placeholder',
-            'points': Decimal(0),
+            "date": arrow.get(0),
+            "description": "placeholder",
+            "points": Decimal(0),
         }
         return [t]
