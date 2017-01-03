bcbb78e2cbf6fcd4d50f160eb500fb56fb10097e
olivere <oe@bink.com>
2017-01-03 15:10:47 +0000

    Fix various flake8 complaints for coop agent solution.

--- app/agents/cooperative.py
+++ app/agents/cooperative.py
@@ -1,10 +1,7 @@
 import re
 from app.agents.base import Miner
-from app.agents.exceptions import LoginError, STATUS_LOGIN_FAILED, AgentError, UNKNOWN, INVALID_MFA_INFO, STATUS_ACCOUNT_LOCKED
-from app.utils import extract_decimal
+from app.agents.exceptions import LoginError, STATUS_LOGIN_FAILED # , STATUS_ACCOUNT_LOCKED
 from decimal import Decimal
-import arrow
-from bs4 import BeautifulSoup
 
 # TODO: add STATUS_ACCOUNT_LOCKED
 # TODO: add negative transaction handling
@@ -16,8 +13,9 @@ class Cooperative(Miner):
 
     def login(self, credentials):
 
-        headers = {'Referer':'https://membership.coop.co.uk/sign-in'}
-        self.browser.open('https://cdns.gigya.com/gs/sso.htm?APIKey=3_150ZZS-BTm2rmX6o2Xe-hzYfRNzKJnG6pAWbx2LkAnJJifFqR0lWrgWkvWvJbVKo&version=2', headers=headers)
+        headers = {'Referer': 'https://membership.coop.co.uk/sign-in'}
+        self.browser.open('https://cdns.gigya.com/gs/sso.htm?APIKey=3_150ZZS-BTm2rmX6o2Xe-hzYfRNzKJnG6pAWbx2LkAnJJifFq'
+                          'R0lWrgWkvWvJbVKo&version=2', headers=headers)
 
         headers = {'Referer': 'https://cdns.gigya.com/gs/APIProxy.html',
                    'Origin': 'https://cdns.gigya.com',
@@ -29,24 +27,28 @@ class Cooperative(Miner):
                 'password': credentials['password'],
                 'authMode': 'cookie',
                 'callback': 'gigya._.apiAdapters.web.callback',
-                'sessionExpiration':'14400',
-                'targetEnv':'jssdk',
-                'includeUserInfo':'true',
-                'gmid':'s%2Bsy1iz%2FuNFBjguOSOuAJKDC0Nyztu8iG1AHFukZRfU%3Ducid:4Y3pB1JNeL1KyJA6mId3gA%3D%3D',
-                'sdk':'js_6.5.23',
-                'format':'jsonp',
-                'context':'R3835806672',
+                'sessionExpiration': '14400',
+                'targetEnv': 'jssdk',
+                'includeUserInfo': 'true',
+                'gmid': 's%2Bsy1iz%2FuNFBjguOSOuAJKDC0Nyztu8iG1AHFukZRfU%3Ducid:4Y3pB1JNeL1KyJA6mId3gA%3D%3D',
+                'sdk': 'js_6.5.23',
+                'format': 'jsonp',
+                'context': 'R3835806672',
                 'utf8': '%E2%9C%93',
                 }
 
-        self.browser.open('https://accounts.eu1.gigya.com/accounts.login?context=R3835806672&saveResponseID=R3835806672', method='post', headers=headers, data=data)
+        self.browser.open('https://accounts.eu1.gigya.com/accounts.login?context=R3835806672&saveResponseID'
+                          '=R3835806672', method='post', headers=headers, data=data)
 
         headers = {'APIKey': '3_XJlLuE7OQJeVauunSRoum1W2XurDGBT6d0qKeA_8T3pHSOhKC4GtFWU-46aAmlsX',
                    'Referer': 'https://cdns.gigya.com/gs/APIProxy.html',
                    'Host': 'accounts.eu1.gigya.com',
                    'Accept': '*/*',
                    }
-        self.browser.open('https://accounts.eu1.gigya.com/socialize.getSavedResponse?APIKey=3_XJlLuE7OQJeVauunSRoum1W2XurDGBT6d0qKeA_8T3pHSOhKC4GtFWU-46aAmlsX&saveResponseID=R3835806672&ucid=4Y3pB1JNeL1KyJA6mId3gA%3D%3D&sdk=js_6.5.23&format=jsonp&callback=gigya._.apiAdapters.web.callback&context=R3835806672', method='get', headers=headers)
+        self.browser.open('https://accounts.eu1.gigya.com/socialize.getSavedResponse?APIKey=3_XJlLuE7OQJeVauunSRoum1W2X'
+                          'urDGBT6d0qKeA_8T3pHSOhKC4GtFWU-46aAmlsX&saveResponseID=R3835806672&ucid=4Y3pB1JNeL1KyJA6mId3'
+                          'gA%3D%3D&sdk=js_6.5.23&format=jsonp&callback=gigya._.apiAdapters.web.callback&context=R38358'
+                          '06672', method='get', headers=headers)
 
         pretty_html = self.browser.parsed.prettify()
         try:
@@ -54,7 +56,8 @@ class Cooperative(Miner):
             if login_fail:
                 raise LoginError(STATUS_LOGIN_FAILED)
         except IndexError:
-            self.open_url('https://membership.coop.co.uk/ajax-calls/validate-signature?UID=cb710208dd084d37a94837d9c5fca0a1&UIDSignature=%2FzSnTuufeLxyzGnwr6w0kdzemhA%3D&signatureTimestamp=1482489324')
+            self.open_url('https://membership.coop.co.uk/ajax-calls/validate-signature?UID=cb710208dd084d37a94837d9c5fc'
+                          'a0a1&UIDSignature=%2FzSnTuufeLxyzGnwr6w0kdzemhA%3D&signatureTimestamp=1482489324')
 
     def balance(self):
         self.open_url('https://membership.coop.co.uk/dashboard')
@@ -84,4 +87,4 @@ class Cooperative(Miner):
 
     def scrape_transactions(self):
         self.open_url("https://www.membership.coop/transactions")
-        return None # self.browser.select("#gridViewMemberTransactions tr")[1:]
+        return None  # self.browser.select("#gridViewMemberTransactions tr")[1:]
