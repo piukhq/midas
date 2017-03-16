f76c75f22407675fca4cdd85811fe7167a98022e
Juan Monetti <jmonetti@bink.com>
2017-03-16 10:36:37 +0000

    Tests for filtering agents errors

--- app/tests/unit/test_cron_results.py
+++ app/tests/unit/test_cron_results.py
@@ -2,7 +2,7 @@ import unittest
 import xmltodict
 import os
 from os.path import join
-from cron_test_results import parse_test_results, generate_message
+from cron_test_results import parse_test_results, generate_message, post_formatted_slack_message
 
 
 class TestCronResults(unittest.TestCase):
@@ -17,28 +17,28 @@ class TestCronResults(unittest.TestCase):
                          'LoginError: Tripped captcha: The agent has tripped the scheme capture code: 532...')
         self.assertEqual(failures['test_nandos']['count'], 4)
 
-    def test_generate_message(self):
+    def test_generate_message_filter_by_causes(self):
+        bad_agents = [
+            {'classname': 'test_captcha_1', 'name': 'avis',
+             'cause': 'LoginError: Tripped captcha: The agent has tripped the scheme capture code'},
+            {'classname': 'test_captcha_2', 'name': 'choicehotels',
+             'cause': 'LoginError: Tripped captcha: The agent has tripped the scheme.'},
+            {'classname': 'test_failure_1', 'name': 'monsoon',
+             'cause': 'TypeError: NoneType object is not subscriptable'},
+            {'classname': 'test_failure_2', 'name': 'hyatt', 'cause': 'AssertionError: LoginError not raised'},
+            {'classname': 'test_failure_3', 'name': 'rewards4golf',
+             'cause': 'Exception: missing the credential ctl00$txtUsername.'},
+            {'classname': 'test_credentials_1', 'name': 'accor',
+             'cause': 'LoginError: Invalid credentials: We could not upda'},
+        ]
+
         with open(join(os.path.dirname(os.path.abspath(__file__)), 'fixtures/example_test_results.xml')) as f:
             test_results = xmltodict.parse(f.read())
 
-        bad_agents = [
-            {'classname': 'test0', 'name': 'test1', 'cause': 'test2'},
-            {'classname': 'test2.5', 'name': 'test3', 'cause': 'test4'},
-        ]
-
         message = generate_message(test_results, bad_agents)
 
-        expected_message = '''*Total errors:* 17/234
-*Time:* 128.817 seconds
-
-*Errors*
->(<http://dev.midas.loyaltyangels.local/resolve_issue/test0|resolve>) test1 - test2
->(<http://dev.midas.loyaltyangels.local/resolve_issue/test2.5|resolve>) test3 - test4
-
-*The Unscrapables*
->_There are currently no notable unscrapable agents._
-
-*End site down:* thai airways, harrods
-http://dev.apollo.loyaltyangels.local/#/exceptions/'''
-
-        self.assertEqual(expected_message, message)
+        self.assertEqual(len(message['failures']), 3, msg="Should filter 3 messages as failures.")
+        self.assertEqual(len(message['captcha']), 2, msg="Should filter 2 messages as captcha.")
+        self.assertEqual(len(message['credentials']), 1, msg="Should filter 3 messages as credentials.")
+        self.assertEquals(message['error_info'], '*Total errors:* 17/234\n*Time:* 128.817 seconds\n')
+        self.assertEquals(message['end_site_down'], 'thai airways, harrods')
