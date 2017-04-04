d8990d61e4b75f282eb308a610b9adccdfbcc0d7
Chris Latham <cl@bink.com>
2017-04-04 09:19:56 +0100

    add flatrock fixes for rewards4racing

--- app/agents/rewards4racing.py
+++ app/agents/rewards4racing.py
@@ -1,5 +1,5 @@
 from app.agents.base import Miner
-from app.agents.exceptions import STATUS_LOGIN_FAILED
+from app.agents.exceptions import STATUS_LOGIN_FAILED, LoginError
 from app.utils import extract_decimal
 from decimal import Decimal
 import arrow
@@ -7,29 +7,70 @@ import re
 
 
 class Rewards4Racing(Miner):
-    point_balance_pattern = re.compile(r'(\d+) points')
-    point_value_pattern = re.compile(r'Worth £(\d+.\d\d)')
+    point_balance_pattern = r'Totalpointspending\:(\d+)'
+    point_value_pattern = r'\,worth(£\d+\.\d+)Missing'
+
+    def set_headers(self):
+        self.headers["Host"] = "www.rewards4racing.com"
+        self.headers["Origin"] = "https://www.rewards4racing.com"
+        self.headers["Referer"] = "https://www.rewards4racing.com/"
+        self.headers["X-MicrosoftAjax"] = "Delta=true"
+        self.headers["X-Requested-With"] = "XMLHttpRequest"
+
+    def get_data(self, credentials):
+        self.open_url('https://www.rewards4racing.com/Home/')
+        login_form = self.browser.get_form('form1')
+        data = {}
+        for field in list(login_form.keys()):
+            data[field] = str(login_form[field].value)
+
+        data["__EVENTTARGET"] = "ctl00$signin$lnkSignIn"
+        data["ctl00$activate$txtConfirmPass"] = ""
+        data["ctl00$activate$txtEmailAddress"] = ""
+        data["ctl00$activate$txtPassword"] = ""
+
+        data["ctl00$ContentPlaceHolder1$modulecontainer$default."
+             "aspx_0_636071242123030000_default.aspx_0$txtSearch"] = ""
+        data["ctl00$ContentPlaceHolder1$modulecontainer$default."
+             "aspx_10_636058356321030000_default.aspx_10$hidKey"] = "CompEmailCapture"
+        data["ctl00$ContentPlaceHolder1$modulecontainer$default."
+             "aspx_10_636058356321030000_default.aspx_10$txtEmailAddress"] = ""
+        data["ctl00$modulecontainer$default.aspx_0_636046966323400000_default.aspx_0$hidKey"] = "newsletter_homepage"
+        data["ctl00$modulecontainer$default.aspx_0_636046966323400000_default.aspx_0$txtEmailAddress"] = ""
+        data["ctl00$scriptmanager"] = "ctl00$signin$udp|ctl00$signin$lnkSignIn"
+
+        data["ctl00$signin$txtEmailAddress"] = credentials["email"]
+        data["ctl00$signin$txtPassword"] = credentials["password"]
+        data["__ASYNCPOST"] = "true"
+
+        data.pop("ctl00$clubSelectPopup$ctl00$btnSubmitClub", None)
+        data.pop("ctl00$clubSelectPopup$ctl00$ddlClubSelector", None)
+        data.pop("ctl00$signin$chkKeepMeLoggedIn", None)
+
+        return data
 
     def login(self, credentials):
-        self.open_url('https://www.rewards4racing.com/Home/')
+        url = "https://www.rewards4racing.com/"
+        data = self.get_data(credentials)
+        self.set_headers()
+        self.open_url(url, method='post', data=data)
 
-        login_form = self.browser.get_form('form1')
-        login_form['ctl00$txtUsername'].value = credentials['email']
-        login_form['ctl00$txtPassword'].value = credentials['password']
-        self.browser.submit_form(login_form, submit=login_form.submit_fields['ctl00$btnSignIn'])
+        generic_error = str(self.browser.select('#signin_pnlGenericError'))
 
-        self.check_error('/Home/',
-                         (('#custLogin div.alert.alert-danger', STATUS_LOGIN_FAILED, 'Error logging into account'), ))
+        if "There was a problem signing you into your account" in generic_error:
+            raise LoginError(STATUS_LOGIN_FAILED)
 
     def balance(self):
         # Despite the fact that this site is a copy-paste job identical to two others, they somehow managed to end up
         # with malformed HTML in this one's point balance, so we have to regex it.
-        pretty_html = self.browser.parsed.prettify()
+        self.open_url("https://www.rewards4racing.com/my-account")
+        pretty_html = self.browser.select(".home-stats")[0].text.strip()\
+            .replace("\n", "").replace("\r", "").replace(" ", "")
 
-        value = extract_decimal(self.point_value_pattern.findall(pretty_html)[0])
+        value = extract_decimal(re.findall(self.point_value_pattern, pretty_html)[0])
 
         return {
-            'points': extract_decimal(self.point_balance_pattern.findall(pretty_html)[0]),
+            'points': extract_decimal(re.findall(self.point_balance_pattern, pretty_html)[0]),
             'value': value,
             'value_label': '£{}'.format(value),
         }
