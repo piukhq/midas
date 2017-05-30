6ec46f60dec9e2d7da5831b40d02dbacd53f5a71
Christian Prior <cprior@bink.com>
2017-05-30 09:46:34 +0000

    better error handling and process closing for hilton agent

--- app/agents/hilton_honors.py
+++ app/agents/hilton_honors.py
@@ -4,7 +4,7 @@ import time
 
 from app.agents.base import Miner
 from app.agents.exceptions import LoginError, AgentError, TRIPPED_CAPTCHA
-from app.agents.exceptions import STATUS_LOGIN_FAILED, UNKNOWN
+from app.agents.exceptions import STATUS_LOGIN_FAILED
 from app.utils import extract_decimal
 from decimal import Decimal
 from selenium import webdriver
@@ -23,19 +23,24 @@ class Hilton(Miner):
         'table[@class="hhonors_table accordion "]/tbody'
 
     def __init__(self, retry_count, scheme_id):
-        self.display = Xvfb()
-        self.display.start()
+        try:
+            self.display = Xvfb()
+            self.display.start()
 
-        firefox_profile = webdriver.FirefoxProfile()
-        firefox_profile.set_preference('permissions.default.image', 2)
-        firefox_profile.set_preference('dom.ipc.plugins.enabled.libflashplayer.so', 'false')
-        self.browser = webdriver.Firefox(firefox_profile=firefox_profile)
+            firefox_profile = webdriver.FirefoxProfile()
+            firefox_profile.set_preference('permissions.default.image', 2)
+            firefox_profile.set_preference('dom.ipc.plugins.enabled.libflashplayer.so', 'false')
+            self.browser = webdriver.Firefox(firefox_profile=firefox_profile)
 
-        self.browser.implicitly_wait(self.MAX_WAITING_TIME_SELENIUM)
+            self.browser.implicitly_wait(self.MAX_WAITING_TIME_SELENIUM)
 
-        self.scheme_id = scheme_id
-        self.retry_count = retry_count
-        self.points = None
+            self.scheme_id = scheme_id
+            self.retry_count = retry_count
+            self.points = None
+
+        except Exception as e:
+            self.quit_selenium()
+            raise e
 
     def login(self, credentials):
         try:
@@ -53,9 +58,9 @@ class Hilton(Miner):
             self.browser.find_element_by_xpath('//input[@id="password"]').send_keys(Keys.RETURN)
             time.sleep(random.randint(10, 20) * 0.1)  # we have less chances to get captcha
 
-        except:
+        except Exception as e:
             self.quit_selenium()
-            raise AgentError(UNKNOWN)
+            raise e
 
         self._find_captcha()
 
@@ -66,15 +71,44 @@ class Hilton(Miner):
 
             self.points = extract_decimal(points_elem.text)
 
-        except:
+        except Exception as e:
             self.browser.implicitly_wait(0)
             error_message = self.browser.find_elements_by_id('invalidCredentials')
             self.quit_selenium()
             if len(error_message) == 0:
-                raise AgentError(UNKNOWN)
+                raise e
 
             else:
+                self.quit_selenium()
                 raise LoginError(STATUS_LOGIN_FAILED)
+
+        # transaction scraping - done here so we can quit selenium no matter what is asked for
+        try:
+            self.browser.get(self.ALL_TRANSACTION_URL)
+
+            self.transactions_list = []
+            table = self.browser.find_element_by_xpath(self.TRANSACTION_TABLE_PATH)
+            rows = table.find_elements_by_tag_name('tr')
+
+            for row in rows:
+                columns = row.find_elements_by_tag_name('td')
+
+                if len(columns) > 1:
+                    # Filter row with 1 column as they are just empty
+                    # Expected structure: [0]Date| [1]''| [2]Description| [3]''|
+                    #                     [4]Points Earned| [5] Miles earned| [6]Action
+                    self.transactions_list.append({
+                        'date': columns[0].text,
+                        'description': columns[2].text,
+                        'points_earned': columns[4].text,
+                        'miles_earned': columns[5].text,
+                        'action': columns[6].text
+                    })
+            self.quit_selenium()
+
+        except Exception as e:
+            self.quit_selenium()
+            raise e
 
     def balance(self):
         return {
@@ -92,34 +126,7 @@ class Hilton(Miner):
         }
 
     def scrape_transactions(self):
-        try:
-            self.browser.get(self.ALL_TRANSACTION_URL)
-
-            transactions = []
-            table = self.browser.find_element_by_xpath(self.TRANSACTION_TABLE_PATH)
-            rows = table.find_elements_by_tag_name('tr')
-
-            for row in rows:
-                columns = row.find_elements_by_tag_name('td')
-
-                if len(columns) > 1:
-                    # Filter row with 1 column as they are just empty
-                    # Expected structure: [0]Date| [1]''| [2]Description| [3]''|
-                    #                     [4]Points Earned| [5] Miles earned| [6]Action
-                    transactions.append({
-                        'date': columns[0].text,
-                        'description': columns[2].text,
-                        'points_earned': columns[4].text,
-                        'miles_earned': columns[5].text,
-                        'action': columns[6].text
-                    })
-            return transactions
-
-        except:
-            raise AgentError(UNKNOWN)
-
-        finally:
-            self.quit_selenium()
+        return self.transactions_list
 
     def _find_captcha(self):
         try:
@@ -132,5 +139,10 @@ class Hilton(Miner):
             pass  # all good captcha not present
 
     def quit_selenium(self):
-        self.browser.quit()
-        self.display.stop()
+        try:
+            self.browser.quit()
+            self.display.stop()
+
+        except Exception as e:
+            self.display.stop()
+            raise e
