b204401f0b3085fbb17e51fb84683493c1011357
Christian Prior <cprior@bink.com>
2017-12-19 11:41:12 +0000

    fixed quotes for victoria agent

--- app/agents/victoria.py
+++ app/agents/victoria.py
@@ -15,36 +15,36 @@ class Victoria(RoboBrowserMiner):
             raise LoginError(STATUS_LOGIN_FAILED)
 
     def set_headers(self):
-        self.headers['Host'] = "www.flytap.com"
-        self.headers['Origin'] = "https://www.flytap.com"
-        self.headers['Referer'] = "https://www.flytap.com/en-us/login"
-        self.headers['Content-Type'] = "application/json"
-        self.headers['X-Requested-With'] = "XMLHttpRequest"
+        self.headers['Host'] = 'www.flytap.com'
+        self.headers['Origin'] = 'https://www.flytap.com'
+        self.headers['Referer'] = 'https://www.flytap.com/en-us/login'
+        self.headers['Content-Type'] = 'application/json'
+        self.headers['X-Requested-With'] = 'XMLHttpRequest'
 
     def _login(self, credentials):
         self.set_headers()
-        url = "https://www.flytap.com/api/LoginPage?sc_mark=US&sc_lang=en-US"
+        url = 'https://www.flytap.com/api/LoginPage?sc_mark=US&sc_lang=en-US'
         data = {
-          "formModel": {
-            "FormData": {
-              "LoginFormID": 0,
-              "Email": credentials['email'],
-              "clientNumber": None,
-              "Password": credentials['password'],
-              "Remember": True,
-              "SocialUserId": None,
-              "SocialProvider": None
+          'formModel': {
+            'FormData': {
+              'LoginFormID': 0,
+              'Email': credentials['email'],
+              'clientNumber': None,
+              'Password': credentials['password'],
+              'Remember': True,
+              'SocialUserId': None,
+              'SocialProvider': None
             }
           }
         }
-        self.open_url(url, method="post", json=data)
+        self.open_url(url, method='post', json=data)
 
     def login(self, credentials):
         self._login(credentials)
         self.check_if_logged_in()
 
     def balance(self):
-        self.browser.open("https://www.flytap.com/en-us/client-area")
+        self.browser.open('https://www.flytap.com/en-us/client-area')
         points = self.browser.select('.center-mode')[0].text
         points = extract_decimal(points)
 
