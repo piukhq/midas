1737a5bf25749e2313880686b1e2744466bf3abb
Francesco Milani <fmilani@bink.com>
2018-03-16 11:34:26 +0000

    add showcase to active.py

--- app/active.py
+++ app/active.py
@@ -90,7 +90,7 @@ AGENTS = {
     'macdonald-hotels': 'the_club.TheClub',
     'marriott-rewards-card': 'marriott.Marriott',
     'club-individual': 'club_individual.ClubIndividual',
-    'harvey-nichols': 'harvey_nichols.HarveyNichols',
+    'showcase': 'showcase.Showcase',
     'krisflyer': 'krisflyer.Krisflyer',
     'stansted-farm': 'my360.My360',
     'the-courtyard': 'my360.My360',
