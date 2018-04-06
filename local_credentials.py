a17b76bbadd930ca0f6f07321feec6dfda37859e
Francesco Milani <fmilani@bink.com>
2018-04-06 10:49:55 +0100

    fix local credentials to create credentials file

--- local_credentials.py
+++ local_credentials.py
@@ -1,13 +1,9 @@
-import os
 import json
 
 import settings
 from app.tests.service.logins import get_credentials
 
 if __name__ == '__main__':
-
+    open(settings.LOCAL_CREDENTIALS_FILE, 'w').close()
     with open(settings.LOCAL_CREDENTIALS_FILE, 'w') as f:
-        if os.path.exists(settings.LOCAL_CREDENTIALS_FILE):
-            f.write('{}')
-
         f.write(json.dumps(get_credentials(), indent=4, sort_keys=True))
