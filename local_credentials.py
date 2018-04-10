2a030e6d6e8b1ed398c2f0007129c3151c43d68a
Christian Prior <cprior@bink.com>
2018-04-10 08:23:02 +0100

    removed duplicate open statement

--- local_credentials.py
+++ local_credentials.py
@@ -4,6 +4,5 @@ import settings
 from app.tests.service.logins import get_credentials
 
 if __name__ == '__main__':
-    open(settings.LOCAL_CREDENTIALS_FILE, 'w').close()
     with open(settings.LOCAL_CREDENTIALS_FILE, 'w') as f:
         f.write(json.dumps(get_credentials(), indent=4, sort_keys=True))
