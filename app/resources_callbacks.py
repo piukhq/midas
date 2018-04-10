57c040f6f1f9b36a320d74cd28177b5cd76aac41
k-aziz <kaziz@bink.com>
2018-04-10 09:10:49 +0100

    Changed endpoint to pass full request object for verifying security

--- app/resources_callbacks.py
+++ app/resources_callbacks.py
@@ -9,14 +9,15 @@ from app.resources import get_agent_class, create_response
 class JoinCallback(Resource):
 
     def post(self, scheme_slug):
-        data = request.get_json()
-        data['record_uid'] = hash_ids.decode(data['record_uid'])[0]
+        hashed_record_uid = request.get_json()['record_uid']
+        record_uid = hash_ids.decode(hashed_record_uid)[0]
+
         agent_class = get_agent_class(scheme_slug)
 
-        key = retry.get_key(agent_class.__name__, data['record_uid'])
+        key = retry.get_key(agent_class.__name__, record_uid)
         retry_count = retry.get_count(key)
-        agent_instance = agent_class(retry_count, data['record_uid'], scheme_slug=scheme_slug)
+        agent_instance = agent_class(retry_count, record_uid, scheme_slug=scheme_slug)
 
-        agent_instance.register(data, inbound=True)
+        agent_instance.register(request, inbound=True)
 
         return create_response({'success': True})
