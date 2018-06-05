350fdef0244ab87ca1da3054b81f631a8ebb906f
Christian Prior <cprior@bink.com>
2018-06-05 16:11:40 +0100

    move healthz url to urls.py

--- app/urls.py
+++ app/urls.py
@@ -2,7 +2,7 @@ from flask_restful_swagger import swagger
 from flask_restful import Api
 
 from app.resources import AgentsErrorResults, Balance, Register, Transactions, ResolveAgentIssue, \
-    AccountOverview, TestResults, AgentQuestions, SingleAgentErrorResult
+    AccountOverview, TestResults, AgentQuestions, SingleAgentErrorResult, Healthz
 from app.resources_callbacks import JoinCallback
 
 api = swagger.docs(Api(), apiVersion='1', api_spec_url="/api/v1/spec")
@@ -20,3 +20,5 @@ api.add_resource(ResolveAgentIssue, '/resolve_issue/<string:classname>', endpoin
 api.add_resource(AgentQuestions, '/agent_questions', endpoint='api.agent_questions')
 
 api.add_resource(JoinCallback, '/join/merchant/<string:scheme_slug>', endpoint="api.join_callback")
+
+api.add_resource(Healthz, '/healthz', endpoint='healthz')
