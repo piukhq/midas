a8279676a6e89848b47c7c31cf04fa8717d294d5
Christian Prior <cprior@bink.com>
2018-06-08 14:57:21 +0100

    new vault url

--- app/security/utils.py
+++ app/security/utils.py
@@ -6,7 +6,7 @@ from flask import request
 
 from app import configuration, AgentException
 from app.agents.exceptions import AgentError
-from settings import VAULT_TOKEN
+from settings import VAULT_TOKEN, VAULT_URL
 
 
 class DoesNotExist(Exception):
@@ -38,7 +38,7 @@ def get_security_credentials(key_items):
     :param key_items: list of dicts {'type': e.g 'bink_public_key', 'storage_key': auto-generated hash from helios}
     :return: key_items: returns same list of dict with added 'value' keys containing actual credential values.
     """
-    client = hvac.Client(token=VAULT_TOKEN)
+    client = hvac.Client(token=VAULT_TOKEN, url=VAULT_URL)
 
     try:
         for key_item in key_items:
