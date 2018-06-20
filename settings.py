758c05da09965644f4b8bd136a08c481a09889f2
Christian Prior <cprior@bink.com>
2018-06-20 14:48:47 +0100

    clean up of redis url on settings.py

--- settings.py
+++ settings.py
@@ -30,16 +30,15 @@ LOCAL = env_var('MIDAS_LOCAL', False)
 AES_KEY = '6gZW4ARFINh4DR1uIzn12l7Mh1UF982L'
 
 REDIS_PASSWORD = env_var('REDIS_PASSWORD', '')
-FORMAT_PASSWORD = '{}@'.format(REDIS_PASSWORD) if REDIS_PASSWORD else ''
 REDIS_HOST = env_var('REDIS_HOST', 'localhost')
 REDIS_PORT = env_var('REDIS_PORT', '6379')
 REDIS_DB = env_var('REDIS_DB', '0')
-REDIS_URL = 'redis://{}{}:{}/{}'.format(
-    FORMAT_PASSWORD,
-    REDIS_HOST,
-    REDIS_PORT,
-    REDIS_DB
-)
+REDIS_URL = 'redis://{password}@{host}:{port}/{db}'.format(**{
+    'password': REDIS_PASSWORD,
+    'host': REDIS_HOST,
+    'port': REDIS_PORT,
+    'db': REDIS_DB
+})
 
 HADES_URL = env_var('HADES_URL', 'http://local.hades.chingrewards.com:8000')
 
