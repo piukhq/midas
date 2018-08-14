609625742871a8276e645389ef1df3e7ee88e106
mmarsh <mmarsh@bink.com>
2018-08-14 16:28:53 +0100

    Flake8 & better coverage

--- app/tests/unit/test_harvey_nic_optins.py
+++ app/tests/unit/test_harvey_nic_optins.py
@@ -1,8 +1,15 @@
 from app.agents.harvey_nichols import HarveyNichols
+from app.tasks.resend_consents import try_consents
 from unittest import mock
 import json
 import unittest
 import settings
+
+
+class MockStore:
+
+    def set(self, *args, **kwargs):
+        pass
 
 
 class MockResponse:
@@ -16,6 +23,23 @@ class MockResponse:
     @property
     def text(self):
         return json.dumps(self.json_data)
+
+
+saved_consents_data = {}
+
+
+class MockedReTryTaskStore:
+
+    def set_task(cls, callback_mod, call_back_func, consents_data):
+        global saved_consents_data
+        saved_consents_data = consents_data
+
+
+def mocked_requests_post_200_on_lastretry(*args, **kwargs):
+    global saved_consents_data
+    if saved_consents_data.get("retries") == 0:
+        return MockResponse({"response": "success", "code": 200}, 200)
+    return MockResponse(None, 400)
 
 
 def mocked_requests_post_400(*args, **kwargs):
@@ -46,7 +70,6 @@ def mock_harvey_nick_post(*args, **kwargs):
 
 
 class TestUserConsents(unittest.TestCase):
-    CELERY_ALWAYS_EAGER = True
 
     def setUp(self):
         settings.CELERY_ALWAYS_EAGER = True
@@ -66,6 +89,7 @@ class TestUserConsents(unittest.TestCase):
         hn.AGENT_TRIES = 1
         hn.HERMES_CONFIRMATION_TRIES = 1
         hn.scheme_id = 123
+        hn.token_store = MockStore
         credentials = {
             'email': 'mytest@localhost.com',
             'password': '12345',
@@ -98,6 +122,7 @@ class TestUserConsents(unittest.TestCase):
         hn.AGENT_TRIES = 1
         hn.HERMES_CONFIRMATION_TRIES = 1
         hn.scheme_id = 123
+        hn.token_store = MockStore
         credentials = {
             'email': 'mytest@localhost.com',
             'password': '12345',
@@ -117,3 +142,113 @@ class TestUserConsents(unittest.TestCase):
         self.assertEqual('{"status": 1}', mock_put.call_args_list[0][1]['data'])
         self.assertEqual('http://127.0.0.1:8000/schemes/user_consent/2', mock_put.call_args_list[1][0][0])
         self.assertEqual('{"status": 1}', mock_put.call_args_list[0][1]['data'])
+
+    @mock.patch('app.tasks.resend_consents.ReTryTaskStore', side_effect=MockedReTryTaskStore)
+    @mock.patch('app.tasks.resend_consents.requests.put', side_effect=mocked_requests_put_200_ok)
+    @mock.patch('app.tasks.resend_consents.requests.post', side_effect=mocked_requests_post_200_on_lastretry)
+    @mock.patch('app.agents.harvey_nichols.HarveyNichols.make_request', side_effect=mock_harvey_nick_post)
+    def test_harvey_nick_mock_login_retry(self, mock_login, mock_post, mock_put, mock_retry):
+        global saved_consents_data
+        user_info = {
+            'scheme_account_id': 123,
+            'status': 'pending'
+        }
+        saved_consents_data = {}
+        hn = HarveyNichols(retry_count=1, user_info=user_info)
+        hn.AGENT_TRIES = 3
+        hn.HERMES_CONFIRMATION_TRIES = 1
+        hn.scheme_id = 123
+        hn.token_store = MockStore
+        credentials = {
+            'email': 'mytest@localhost.com',
+            'password': '12345',
+            'consents': [
+                {"id": 1, "slug": "email_optin", "value": True, "created_on": "2018-05-11 12:42"},
+                {"id": 2, "slug": "push_optin", "value": False, "created_on": "2018-05-11 12:44"},
+            ]
+
+        }
+        with self.assertLogs() as logs:
+            hn._login(credentials)
+            self.assertEqual('https://admin.uat.harveynichols.com/preferences/create',
+                             mock_post.call_args_list[0][0][0])
+
+            self.assertEqual('{"enactor_id": "2601507998647", "email_optin": true, "push_optin": false}',
+                             mock_post.call_args_list[0][1]["data"])
+
+            # after first try should have empty put list as consents not sent (tries == 3)
+            self.assertListEqual(mock_put.call_args_list, [])
+
+            self.assertIn("400", logs.output[0])
+            self.assertIn("midas_logger", logs.output[0])
+            self.assertIn("agent", logs.output[0])
+            self.assertTrue(mock_retry.called)
+            self.assertFalse(mock_put.called)
+
+            # retry the agent consent 1st time  - should fail
+            saved_consents_data["retries"] -= 1
+            try_consents(saved_consents_data)
+            self.assertListEqual(mock_put.call_args_list, [])
+
+            # retry the agent consent 2nd time  - should succeed ie last retry
+            saved_consents_data["retries"] -= 1
+            try_consents(saved_consents_data)
+
+            self.assertEqual('http://127.0.0.1:8000/schemes/user_consent/1', mock_put.call_args_list[0][0][0])
+            self.assertEqual('{"status": 1}', mock_put.call_args_list[0][1]['data'])
+            self.assertEqual('http://127.0.0.1:8000/schemes/user_consent/2', mock_put.call_args_list[1][0][0])
+            self.assertEqual('{"status": 1}', mock_put.call_args_list[0][1]['data'])
+
+    @mock.patch('app.tasks.resend_consents.ReTryTaskStore', side_effect=MockedReTryTaskStore)
+    @mock.patch('app.tasks.resend_consents.requests.put', side_effect=mocked_requests_put_200_ok)
+    @mock.patch('app.tasks.resend_consents.requests.post', side_effect=mocked_requests_post_400)
+    @mock.patch('app.agents.harvey_nichols.HarveyNichols.make_request', side_effect=mock_harvey_nick_post)
+    def test_harvey_nick_mock_login_agent_fails(self, mock_login, mock_post, mock_put, mock_retry):
+        user_info = {
+            'scheme_account_id': 123,
+            'status': 'pending'
+        }
+        hn = HarveyNichols(retry_count=1, user_info=user_info)
+        hn.AGENT_TRIES = 3
+        hn.HERMES_CONFIRMATION_TRIES = 1
+        hn.scheme_id = 123
+        hn.token_store = MockStore
+        credentials = {
+            'email': 'mytest@localhost.com',
+            'password': '12345',
+            'consents': [
+                {"id": 1, "slug": "email_optin", "value": True, "created_on": "2018-05-11 12:42"},
+                {"id": 2, "slug": "push_optin", "value": False, "created_on": "2018-05-11 12:44"},
+            ]
+
+        }
+        with self.assertLogs() as logs:
+            hn._login(credentials)
+            self.assertEqual('https://admin.uat.harveynichols.com/preferences/create',
+                             mock_post.call_args_list[0][0][0])
+
+            self.assertEqual('{"enactor_id": "2601507998647", "email_optin": true, "push_optin": false}',
+                             mock_post.call_args_list[0][1]["data"])
+
+            # after first try should have empty put list as consents not sent (tries == 3)
+            self.assertListEqual(mock_put.call_args_list, [])
+
+            self.assertIn("400", logs.output[0])
+            self.assertIn("midas_logger", logs.output[0])
+            self.assertIn("agent", logs.output[0])
+            self.assertTrue(mock_retry.called)
+            self.assertFalse(mock_put.called)
+
+            # retry the agent consent 1st time  - should fail
+            saved_consents_data["retries"] -= 1
+            try_consents(saved_consents_data)
+            self.assertListEqual(mock_put.call_args_list, [])
+
+            # retry the agent consent 2nd time  - should fail again and as last try log set consensts failed
+            saved_consents_data["retries"] -= 1
+            try_consents(saved_consents_data)
+
+            self.assertEqual('http://127.0.0.1:8000/schemes/user_consent/1', mock_put.call_args_list[0][0][0])
+            self.assertEqual('{"status": 2}', mock_put.call_args_list[0][1]['data'])
+            self.assertEqual('http://127.0.0.1:8000/schemes/user_consent/2', mock_put.call_args_list[1][0][0])
+            self.assertEqual('{"status": 2}', mock_put.call_args_list[0][1]['data'])
