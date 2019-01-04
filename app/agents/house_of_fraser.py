444b09a1c50df12371a90d3e378a650bdaaa61c0
Cailet Latham <cailet@bink.com>
2019-01-04 14:45:26 +0000

    Fixed House Of Fraser agent

--- app/agents/house_of_fraser.py
+++ app/agents/house_of_fraser.py
@@ -1,33 +1,34 @@
 from app.agents.base import RoboBrowserMiner
-from app.agents.exceptions import LoginError, STATUS_LOGIN_FAILED, UNKNOWN
-from app.utils import extract_decimal
+from app.agents.exceptions import LoginError, STATUS_LOGIN_FAILED
+from decimal import Decimal
 import re
+import json
 
 
 class HouseOfFraser(RoboBrowserMiner):
     error_message_pattern = re.compile(r'showErrorMsg\("","(.*)"\)')
 
     def login(self, credentials):
-        self.open_url('https://www.houseoffraser.co.uk/on/demandware.store/Sites-hof-Site/default/Loyalty-ViewSummary')
-
-        login_form = self.browser.get_form('dwfrm_login')
-        login_form['dwfrm_login_username'].value = credentials['email']
-        login_form['dwfrm_login_password'].value = credentials['password']
-        self.browser.submit_form(login_form, login_form.submit_fields['dwfrm_login_login'])
-
-        # The error message box on the page is filled out with javascript.
-        error_message = self.error_message_pattern.findall(self.browser.parsed.text)
-        if error_message:
-            if error_message[0].startswith('Uh oh. The email/password combination'):
-                raise LoginError(STATUS_LOGIN_FAILED)
-            else:
-                raise LoginError(UNKNOWN)
+        form = 'https://www.houseoffraser.co.uk/account/validatelogin'
+        self.open_url(
+            form,
+            method='post',
+            json={
+                'Email': credentials['email'],
+                'Password': credentials['password']
+            })
 
     def balance(self):
-        loyalty_data_elements = self.browser.select('td.hof-title.text-align-right.compact')
-        value = extract_decimal(loyalty_data_elements[1].text.strip())
+        self.open_url('https://www.houseoffraser.co.uk/recognition/recognitionsummary')
+        match = re.search(
+            r"^\s*recognitionsummary: new hof\.models\.RecognitionSummaryViewModel\((.*)\),.*$",
+            self.browser.response.text,
+            re.MULTILINE
+        )
+        data = json.loads(match.groups()[0])
+        value = Decimal(data['RewardsBalance'])
         return {
-            'points': extract_decimal(loyalty_data_elements[0].text.strip()),
+            'points': Decimal(data['PointBalance']),
             'value': value,
             'value_label': '£{}'.format(value),
         }
@@ -38,3 +39,8 @@ class HouseOfFraser(RoboBrowserMiner):
 
     def scrape_transactions(self):
         return []
+
+    def _raise_agent_exception(self, exc):
+        if exc.response.status_code == 422:
+            raise LoginError(STATUS_LOGIN_FAILED)
+        super()._raise_agent_exception(exc)
