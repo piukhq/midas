87a47f61d1a727651251a5a0f798d1bd90a66c37
Kash <kashim_aziz@yahoo.co.uk>
2019-01-25 17:29:56 +0000

    Use UserTokenStore to store and retrieve auth tokens

--- app/agents/cooperative_merchant_integration.py
+++ app/agents/cooperative_merchant_integration.py
@@ -1,13 +1,21 @@
+import json
 from decimal import Decimal
 
 import arrow
+import time
+from gaia.user_token import UserTokenStore
 
 from app.agents.base import MerchantApi
+from settings import REDIS_URL
 
 
 class Cooperative(MerchantApi):
     API_KEY = 'awmHjJzzfV3YMUuJdcmd56PKRIzg6KAg1WKn94Ds'
+    AUTH_TOKEN_TIMEOUT = 3600
+
     identifier_type = ['card_number', 'merchant_scheme_id2']
+
+    token_store = UserTokenStore(REDIS_URL)
 
     def balance(self):
         value = Decimal(self.result['alt_value'])
@@ -45,13 +53,35 @@ class Cooperative(MerchantApi):
         return merchant_ids
 
     def apply_security_measures(self, json_data, security_service, security_credentials):
+        auth_token_header = "Access Token"
+        try:
+            access_token = self.token_store.get(self.scheme_id)
 
-        # TODO: if access token is in UserTokenStore and is valid, use that, or retrieve token.
-        request = super().apply_security_measures(json_data, security_service, security_credentials)
+            if self._token_is_valid(access_token['timestamp']):
+                request = {
+                    "json": json.loads(json_data),
+                    "headers": {
+                        auth_token_header: "{} {}".format(security_credentials['prefix'], access_token['token'])
+                    }
+                }
+            else:
+                raise self.token_store.NoSuchToken
 
-        request['headers']['Access Token'] = request['headers'].pop('Authorization')
+        except self.token_store.NoSuchToken:
+            request = super().apply_security_measures(json_data, security_service, security_credentials)
+            timestamp = time.time()
+
+            request['headers'][auth_token_header] = request['headers'].pop('Authorization')
+            self.token_store.set(
+                self.scheme_id,
+                {'token': request['headers'][auth_token_header], 'timestamp': timestamp}
+            )
+
         request['headers']['X-API-KEY'] = Cooperative.API_KEY
 
-        # TODO: store access token in UserTokenStore
-
         return request
+
+    @staticmethod
+    def _token_is_valid(timestamp):
+        current_time = time.time()
+        return (current_time - timestamp) > Cooperative.AUTH_TOKEN_TIMEOUT
