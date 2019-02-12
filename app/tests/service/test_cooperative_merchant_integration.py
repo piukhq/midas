a8e2997bea9da2f7f3c906fe9a94272c2697cd95
k-aziz <kaziz@bink.com>
2019-02-12 16:38:01 +0000

    Additional Coop tests

--- app/tests/service/test_cooperative_merchant_integration.py
+++ app/tests/service/test_cooperative_merchant_integration.py
@@ -1,7 +1,10 @@
 import json
 import unittest
+from unittest.mock import MagicMock, patch
+
 from app.agents.cooperative_merchant_integration import Cooperative
 from app.agents import schemas
+from app.configuration import Configuration
 from app.tests.service.logins import CREDENTIALS, AGENT_CLASS_ARGUMENTS, AGENT_CLASS_ARGUMENTS_FOR_VALIDATE
 from gaia.user_token import UserTokenStore
 from settings import REDIS_URL
@@ -12,8 +15,13 @@ class TestCooperative(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.c = Cooperative(*AGENT_CLASS_ARGUMENTS, scheme_slug='test-co-op')
-        cls.c.attempt_login(CREDENTIALS['the-co-operative-membership'])
+        credentials = CREDENTIALS['test-co-op']
+
         cls.token_store = UserTokenStore(REDIS_URL)
+        for scope in cls.c.journey_to_scope.values():
+            cls.token_store.delete(scope)
+
+        cls.c.attempt_login(credentials)
 
     def test_transactions(self):
         transactions = self.c.transactions()
@@ -25,27 +33,105 @@ class TestCooperative(unittest.TestCase):
         schemas.balance(balance)
 
     def test_auth_token_storage(self):
-        data = json.loads(self.token_store.get(1))
+        update_scope = self.c.journey_to_scope[Configuration.UPDATE_HANDLER]
+        data = json.loads(self.token_store.get(update_scope))
 
         self.assertNotEqual(data.get('token'), None)
         self.assertNotEqual(data.get('timestamp'), None)
 
-    def tearDownClass(cls):
-        cls.token_store.delete(1)
+    @patch.object(Cooperative, '_get_auth_token')
+    @patch.object(Cooperative, '_refresh_auth_token')
+    def test_get_auth_headers_gets_token_if_not_in_token_store(self, mock_refresh_token, mock_get_token):
+        mock_refresh_token.return_value = {'token': 'refreshed_token', 'timestamp': 1234556789}
+        mock_get_token.side_effect = self.token_store.NoSuchToken
+
+        expected_result = {
+            'Authorization': 'Bearer refreshed_token',
+            'X-API-KEY': Cooperative.API_KEY
+        }
+
+        result = self.c._get_auth_headers('check_card')
+
+        self.assertTrue(mock_get_token.called)
+        self.assertTrue(mock_refresh_token.called)
+        self.assertEqual(result, expected_result)
+
+    @patch.object(Cooperative, '_get_auth_token')
+    @patch.object(Cooperative, '_refresh_auth_token')
+    def test_get_auth_headers_refreshes_expired_token(self, mock_refresh_token, mock_get_token):
+        mock_refresh_token.return_value = {'token': 'refreshed_token', 'timestamp': 1234556789}
+        mock_get_token.return_value = {'token': 'stored_token', 'timestamp': 1234556789}
+
+        expected_result = {
+            'Authorization': 'Bearer refreshed_token',
+            'X-API-KEY': Cooperative.API_KEY
+        }
+
+        result = self.c._get_auth_headers('check_card')
+
+        self.assertTrue(mock_get_token.called)
+        self.assertTrue(mock_refresh_token.called)
+        self.assertEqual(result, expected_result)
 
 
 class TestCooperativeValidate(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.i = Cooperative(*AGENT_CLASS_ARGUMENTS_FOR_VALIDATE, scheme_slug='the-co-operative-membership')
-        credentials = CREDENTIALS['the-co-operative-membership']
-        credentials.pop('merchant_identifier', None)
-
-        cls.i.attempt_login(CREDENTIALS['the-co-operative-membership'])
+        cls.c = Cooperative(*AGENT_CLASS_ARGUMENTS_FOR_VALIDATE, scheme_slug='test-co-op')
+        cls.credentials = CREDENTIALS['test-co-op']
+        cls.credentials.pop('merchant_identifier', None)
 
     def test_validate(self):
-        balance = self.i.balance()
+        self.c.attempt_login(self.credentials)
+        balance = self.c.balance()
         schemas.balance(balance)
+
+    def test_validate_error_handler_raises_unknown_for_bad_status_codes(self):
+        response = MagicMock()
+        response.text = ''
+        response.headers = {}
+        response.history = False
+
+        expected_response_json = (
+            '{"error_codes": [{"code": "UNKNOWN", '
+            '"description": "An unknown error has occurred with status code 400"}]}'
+        )
+
+        response.status_code = 400
+        response_json = self.c._validate_error_handler(response)
+
+        self.assertEqual(expected_response_json, response_json)
+
+    def test_validate_error_handler_raises_login_failed_for_unverified_card_number(self):
+        response = MagicMock()
+        response.text = ''
+        response.headers = ''
+        response.history = False
+
+        expected_response_json = (
+            '{"error_codes": [{"code": "STATUS_LOGIN_FAILED",'
+            ' "description": "Invalid credentials"}]}'
+        )
+
+        response.status_code = 200
+        response_json = self.c._validate_error_handler(response)
+
+        self.assertEqual(expected_response_json, response_json)
+
+    def test_join_error_handler_raises_join_error_for_400_status(self):
+        response = MagicMock()
+        response.text = ''
+        response.headers = ''
+        response.history = False
+
+        expected_response_json = (
+            '{"error_codes": [{"code": "JOIN_ERROR", "description": "Duplicate registration."}]}'
+        )
+
+        response.status_code = 400
+        response_json = self.c._join_error_handler(response)
+
+        self.assertEqual(expected_response_json, response_json)
 
 
 if __name__ == '__main__':
