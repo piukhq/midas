8a869422a05ce20a73994cf43d8c3dd08349b6d7
David Felce <dfelce@bink.com>
2021-01-28 19:07:16 +0000

    MER-788: Black formatting

--- app/tests/unit/test_prometheus.py
+++ app/tests/unit/test_prometheus.py
@@ -10,20 +10,20 @@ class TestPrometheus(TestCase):
     # conflicts in the registry
     prometheus = PrometheusManager()
 
-    @mock.patch('app.prometheus.Counter.inc', autospec=True)
+    @mock.patch("app.prometheus.Counter.inc", autospec=True)
     def test_log_in_success(self, mock_prometheus_counter_inc):
         """
-        Test the login success counter increment
+        Test the login success counter increments
         """
         settings.PUSH_PROMETHEUS_METRICS = False  # Disable the attempted push
         signal("log-in-success").send(self, slug="test-prometheus")
 
         self.assertTrue(mock_prometheus_counter_inc.called)
 
-    @mock.patch('app.prometheus.Counter.inc', autospec=True)
+    @mock.patch("app.prometheus.Counter.inc", autospec=True)
     def test_log_in_fail(self, mock_prometheus_counter_inc):
         """
-        Test the login fail counter increment
+        Test the login fail counter increments
         """
         settings.PUSH_PROMETHEUS_METRICS = False  # Disable the attempted push
         signal("log-in-fail").send(self, slug="test-prometheus")
