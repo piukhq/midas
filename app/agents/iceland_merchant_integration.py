68e98f1a275a8557f384e2f955814209a3c51b32
rahimakarbary <rkarbary@bink.com>
2021-01-28 16:35:40 +0200

    Added Handler for Add Journey

--- app/agents/iceland_merchant_integration.py
+++ app/agents/iceland_merchant_integration.py
@@ -6,4 +6,4 @@ class Iceland(MerchantAPIGeneric):
 
     def __init__(self, retry_count, user_info, scheme_slug=None, config=None, consents_data=None):
         super().__init__(retry_count, user_info, scheme_slug, config, consents_data)
-        self.audit_logger.journeys = (Configuration.JOIN_HANDLER,)
+        self.audit_logger.journeys = (Configuration.JOIN_HANDLER, Configuration.VALIDATE_HANDLER)
