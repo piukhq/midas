3c49ec82f91c5e8dbcc7d695c84a73e2d2f0adfc
mlatham <ml@bink.com>
2021-05-14 15:53:55 +0100

    MER-1105 bpl callback unit test

--- app/bpl_callback.py
+++ app/bpl_callback.py
@@ -1,6 +1,7 @@
 import json
 import requests
 
+import settings
 from flask import request
 from flask_restful import Resource
 from app import configuration
@@ -12,7 +13,6 @@ from app.security.utils import get_security_agent
 from azure_oidc.integrations.flask_decorator import FlaskOIDCAuthDecorator
 from azure_oidc import OIDCConfig
 from app.utils import get_headers
-from settings import HERMES_URL
 
 tenant_id = "a6e2367a-92ea-4e5a-b565-723830bcc095"
 config = OIDCConfig(
@@ -20,28 +20,53 @@ config = OIDCConfig(
     issuer=f"https://sts.windows.net/{tenant_id}/",
     audience="api://midas-nonprod",
 )
-requires_auth = FlaskOIDCAuthDecorator(config)
+
+_requires_auth = None
+
+
+def _nop_decorator(*args, **kwargs):
+    return lambda fn: fn
+
+
+def auth_decorator():
+    if settings.API_AUTH_ENABLED is False:
+        return _nop_decorator
+
+    global _requires_auth
+    if _requires_auth is None:
+        _requires_auth = FlaskOIDCAuthDecorator(config)
+    return _requires_auth
+
+
+requires_auth = auth_decorator()
 
 
 class JoinCallbackBpl(Resource):
     @requires_auth()
     def post(self, scheme_slug):
-        config = configuration.Configuration(scheme_slug, Configuration.JOIN_HANDLER)
+
+        data = self.json_data(scheme_slug, request)
+
+        self.update_hermes(data)
+
+        return create_response({"success": True})
+
+    def json_data(self, scheme_slug, callback_request):
+        join_config = configuration.Configuration(scheme_slug, Configuration.JOIN_HANDLER)
         security_agent = get_security_agent(
-            config.security_credentials["inbound"]["service"], config.security_credentials
+            join_config.security_credentials["inbound"]["service"], config.security_credentials
         )
 
-        data = json.loads(security_agent.decode(request.headers, request.get_data().decode("utf8")))
+        return json.loads(security_agent.decode(callback_request.headers, callback_request.get_data().decode("utf8")))
 
+    def update_hermes(self, data):
         scheme_account_id = hash_ids.decode(data["third_party_identifier"])
-
         identifier = {"card_number": data["account_number"], "merchant_identifier": data["UUID"]}
         identifier_data = json.dumps(identifier, cls=JsonEncoder)
         headers = get_headers("success")
 
         requests.put(
-            "{}/schemes/accounts/{}/credentials".format(HERMES_URL, scheme_account_id[0]),
+            "{}/schemes/accounts/{}/credentials".format(settings.HERMES_URL, scheme_account_id[0]),
             data=identifier_data,
             headers=headers,
         )
-        return create_response({"success": True})
--- app/tests/unit/test_bpl.py
+++ app/tests/unit/test_bpl.py
@@ -1,31 +1,32 @@
-import json
-
+import settings
 from flask_testing import TestCase
-from app import create_app
 from unittest import mock
 
+settings.API_AUTH_ENABLED = False
+from app.bpl_callback import JoinCallbackBpl
+from app import create_app
 
-data = json.dumps({"UUID": "7e54d768-033e-40fa-999a-76c21bdd9c42",
-                        "email": "ncostaa@bink.com",
-                        "account_number": 56789,
-                        "third_party_identifier": "8v5zjgey0xd7k618x43wmpo2139lq4r8"
-                        })
 
-headers = {'Content-type': 'application/json',
-           'transaction': "success",
-           'Authorization': 'Bearer ' + "sgadadiuaHOAHSOOAH"}
+data = {"UUID": "7e54d768-033e-40fa-999a-76c21bdd9c42",
+        "email": "ncostaa@bink.com",
+        "account_number": 56789,
+        "third_party_identifier": "8v5zjgey0xd7k618x43wmpo2139lq4r8"
+        }
+
+headers = {'Content-type': 'application/json'}
 
 
 class TestBplCallback(TestCase):
 
     def create_app(self):
-        return create_app(self, )
+        return create_app(self)
 
-    @mock.patch('app.bpl_callback.requires_auth', return_value=True)
-    def test_post(self, mock_flask_oidc):
-
+    @mock.patch.object(JoinCallbackBpl, 'update_hermes')
+    @mock.patch.object(JoinCallbackBpl, 'json_data')
+    def test_post(self, mock_json_data, mock_update_hermes):
+        mock_json_data.return_value = data
         url = "join/bpl/bpl-trenette"
         response = self.client.post(url, data=data, headers=headers)
 
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json, {'message': 'success'})
+        self.assertEqual(response.json, {'success': True})
