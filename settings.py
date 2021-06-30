ba255dba0e97222a885dbde1a9c7f5ef59f23d90
Nick Costa <ncosta@bink.com>
2021-06-30 16:54:45 +0100

    updated get_env variable settings

--- settings.py
+++ settings.py
@@ -27,8 +27,8 @@ def getenv(key: str, default: str = None, conv: t.Callable = str, required: bool
         return None
 
 
-DEV_HOST = getenv('DEV_HOST', '0.0.0.0')
-DEV_PORT = getenv('DEV_PORT', '8000')
+DEV_HOST = getenv('DEV_HOST', default='0.0.0.0')
+DEV_PORT = getenv('DEV_PORT', default='8000', conv=int)
 
 logging.basicConfig(
     level=logging.INFO,
@@ -47,14 +47,14 @@ DEBUG = getenv('MIDAS_DEBUG', default="false")
 LOCAL = getenv('MIDAS_LOCAL', default="false")
 AES_KEY = '6gZW4ARFINh4DR1uIzn12l7Mh1UF982L'
 
-REDIS_PASSWORD = getenv('REDIS_PASSWORD', '')
-REDIS_HOST = getenv('REDIS_HOST', 'localhost')
-REDIS_PORT = getenv('REDIS_PORT', '6379')
-REDIS_DB = getenv('REDIS_DB', '0')
+REDIS_PASSWORD = getenv('REDIS_PASSWORD', default='')
+REDIS_HOST = getenv('REDIS_HOST', default='localhost')
+REDIS_PORT = getenv('REDIS_PORT', default='6379')
+REDIS_DB = getenv('REDIS_DB', default='0')
 REDIS_URL = getenv('REDIS_URL', f'redis://:{REDIS_PASSWORD}@{REDIS_HOST}:{REDIS_PORT}/{REDIS_DB}')
 
-RETRY_PERIOD = getenv('RETRY_PERIOD', '1800')
-REDIS_CELERY_DB = getenv('REDIS_CELERY_DB', '1')
+RETRY_PERIOD = getenv('RETRY_PERIOD', default='1800', conv=int)
+REDIS_CELERY_DB = getenv('REDIS_CELERY_DB', default='1')
 CELERY_BROKER_URL = getenv(
     'AMQP_DSN',
     f'redis://:{REDIS_PASSWORD}@{REDIS_HOST}:{REDIS_PORT}/{REDIS_CELERY_DB}'
@@ -63,7 +63,7 @@ CELERY_TASK_SERIALIZER = 'json'
 CELERYBEAT_SCHEDULE = {
     'retry_tasks': {
         'task': 'app.tasks.resend.retry_tasks',
-        'schedule': int(RETRY_PERIOD),
+        'schedule': RETRY_PERIOD,
         'args': ()
     }
 }
@@ -71,12 +71,12 @@ CELERY_IMPORTS = [
     'app.tasks.resend'
 ]
 
-HADES_URL = getenv('HADES_URL', 'http://local.hades.chingrewards.com:8000')
-HERMES_URL = getenv('HERMES_URL', 'http://local.hermes.chingrewards.com:8000')
-HELIOS_URL = getenv('HELIOS_URL', 'https://api.bink-dev.xyz/dashboard')
-HEARTBEAT_URL = getenv('HEARTBEAT_URL', 'https://hchk.io/976b50d5-1616-4c7e-92ac-6e05e0916e82')
-CONFIG_SERVICE_URL = getenv('CONFIG_SERVICE_URL', '')
-ATLAS_URL = getenv('ATLAS_URL', 'http://localhost:8100')
+HADES_URL = getenv('HADES_URL', default='http://local.hades.chingrewards.com:8000')
+HERMES_URL = getenv('HERMES_URL', default='http://local.hermes.chingrewards.com:8000')
+HELIOS_URL = getenv('HELIOS_URL', default='https://api.bink-dev.xyz/dashboard')
+HEARTBEAT_URL = getenv('HEARTBEAT_URL', default='https://hchk.io/976b50d5-1616-4c7e-92ac-6e05e0916e82')
+CONFIG_SERVICE_URL = getenv('CONFIG_SERVICE_URL', default='')
+ATLAS_URL = getenv('ATLAS_URL', default='http://localhost:8100')
 
 SERVICE_API_KEY = 'F616CE5C88744DD52DB628FAD8B3D'
 
@@ -102,19 +102,19 @@ if SENTRY_DSN:
 
 PROPAGATE_EXCEPTIONS = True
 
-INFLUX_HOST = getenv('INFLUX_HOST', '192.168.1.53')
-INFLUX_PORT = getenv('INFLUX_PORT', '8086')
-INFLUX_USER = getenv('INFLUX_USER', 'root')
-INFLUX_PASSWORD = getenv('INFLUX_PASSWORD', 'root')
-INFLUX_DATABASE = getenv('INFLUX_DATABASE', 'test_results')
+INFLUX_HOST = getenv('INFLUX_HOST', default='192.168.1.53')
+INFLUX_PORT = getenv('INFLUX_PORT', default='8086', conv=int)
+INFLUX_USER = getenv('INFLUX_USER', default='root')
+INFLUX_PASSWORD = getenv('INFLUX_PASSWORD', default='root')
+INFLUX_DATABASE = getenv('INFLUX_DATABASE', default='test_results')
 
 MAX_VALUE_LABEL_LENGTH = 11
 
-HELIOS_DB_USER = getenv('HELIOS_DB_USER', 'helios')
-HELIOS_DB_PASS = getenv('HELIOS_DB_PASS', 'j8NUz3vzPSn$')
-HELIOS_DB_HOST = getenv('HELIOS_DB_HOST', '127.0.0.1')
-HELIOS_DB_PORT = getenv('HELIOS_DB_PORT', '5432')
-HELIOS_DB_NAME = getenv('HELIOS_DB_NAME', 'helios')
+HELIOS_DB_USER = getenv('HELIOS_DB_USER', default='helios')
+HELIOS_DB_PASS = getenv('HELIOS_DB_PASS', default='j8NUz3vzPSn$')
+HELIOS_DB_HOST = getenv('HELIOS_DB_HOST', default='127.0.0.1')
+HELIOS_DB_PORT = getenv('HELIOS_DB_PORT', default='5432', conv=int)
+HELIOS_DB_NAME = getenv('HELIOS_DB_NAME', default='helios')
 
 HELIOS_DB_URI = (
     'postgresql+psycopg2://'
@@ -124,13 +124,13 @@ HELIOS_DB_URI = (
 CREDENTIALS_LOCAL = getenv('CREDENTIALS_LOCAL', default="false")
 LOCAL_CREDENTIALS_FILE = os.path.join(APP_DIR, 'app', 'tests', 'service', 'credentials', 'credentials.json')
 
-VAULT_URL = getenv('VAULT_URL', 'http://localhost:8200')
-VAULT_SECRETS_PATH = getenv('VAULT_SECRETS_PATH', '')
+VAULT_URL = getenv('VAULT_URL', default='http://localhost:8200')
+VAULT_SECRETS_PATH = getenv('VAULT_SECRETS_PATH', default='')
 # Vault settings for merchant api security credential storage
-VAULT_TOKEN = getenv('VAULT_TOKEN', 'myroot')
+VAULT_TOKEN = getenv('VAULT_TOKEN', default='myroot')
 
-MAX_SELENIUM_BROWSERS = getenv('MAX_SELENIUM_BROWSERS', '5')
-SELENIUM_BROWSER_TIMEOUT = getenv('SELENIUM_BROWSER_TIMEOUT', '300')
+MAX_SELENIUM_BROWSERS = getenv('MAX_SELENIUM_BROWSERS', default='5', conv=int)
+SELENIUM_BROWSER_TIMEOUT = getenv('SELENIUM_BROWSER_TIMEOUT', default='300', conv=int)
 
 BACK_OFF_COOLDOWN = 120
 
