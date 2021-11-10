40671efbeba7bcdb15282d78eafc6e4dfaeb274f
Michal Jozwiak <mjozwiak@bink.com>
2021-11-10 18:20:15 +0000

    add Squaremeal agent

--- app/agents/squaremeal.py
+++ app/agents/squaremeal.py
@@ -17,10 +17,10 @@ class SquaremealBase(ApiMiner):
             settings.CONFIG_SERVICE_URL,
         )
         self.base_url = config.merchant_url
-        # self.auth = config.security_credentials["outbound"]["credentials"][0]["value"]["token"]
-        # self.secondary_key = config.security_credentials["outbound"]["credentials"][1]["value"]["token"]
-        # self.headers = {"bpl-user-channel": self.channel, "Authorization": f"Token {self.auth}", "Secondary-Key": {self.secondary_key}}
+        self.auth = config.security_credentials["outbound"]["credentials"][0]["value"]["url"]
+        self.secondary_key = str(config.security_credentials["outbound"]["credentials"][0]["value"]["secondary-key"])
         super().__init__(retry_count, user_info, scheme_slug=scheme_slug)
+        self.headers = {"Authorization": "", "Secondary-Key": self.secondary_key}
 
 
 class Squaremeal(SquaremealBase):
@@ -32,27 +32,27 @@ class Squaremeal(SquaremealBase):
         consents = credentials.get("consents", [])
         url = f"{self.base_url}register"
         payload = {
-            "email": "",
-            "password": "",
-            "FirstName": "",
-            "LastName": "",
+            "email": credentials["email"],
+            "password": credentials["password"],
+            "FirstName": credentials["first_name"],
+            "LastName": credentials["last_name"],
             "Source": "Bink"
         }
         try:
             resp = self.make_request(url, method="post", json=payload)
+            resp_json = resp.json()
         except (LoginError, AgentError) as ex:
             self.handle_errors(ex.response.json()["error"], unhandled_exception_code=GENERAL_ERROR)
 
-        # newsletter_optin = self._get_newsletter_consent(self, consents)
-        newsletter_optin = False
-
+        user_id = resp_json["UserId"]
+        newsletter_optin = consents[0]["value"] if consents else False
         if newsletter_optin:
-            url = f"{self.base_url}update/newsletter"
-            payload = {
+            url = f"{self.base_url}update/newsletters/{user_id}"
+            payload = [{
                 "Newsletter": "Weekly restaurants and bars news",
                 "Subscription": "true"
-            }
+            }]
             try:
-                self.make_request(url, method="post", json=payload)
-            except (LoginError, AgentError) as ex:
+                self.make_request(url, method="put", json=payload)
+            except Exception as ex:
                 self.handle_errors(ex.response.json()["error"], unhandled_exception_code=GENERAL_ERROR)
