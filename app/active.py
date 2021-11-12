2fcbb6a1633717a32378b63a67ddf96edc6c38be
Michal Jozwiak <mjozwiak@bink.com>
2021-11-12 13:06:41 +0000

    change squaremeal slug

--- app/active.py
+++ app/active.py
@@ -15,5 +15,5 @@ AGENTS = {
     "whsmith-rewards-mock": "mock_agents.MockAgentWHS",
     "wasabi-club": "acteol.Wasabi",
     "bpl-trenette": "bpl.Trenette",
-    "squaremeal-restaurant-rewards": "squaremeal.Squaremeal",
+    "squaremeal": "squaremeal.Squaremeal",
 }
