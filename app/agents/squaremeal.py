44c3c3550de703a452a1449db74ec87521fed4db
Michal Jozwiak <mjozwiak@bink.com>
2021-11-12 12:05:26 +0000

    add authentication

--- app/agents/squaremeal.py
+++ app/agents/squaremeal.py
@@ -1,6 +1,11 @@
 import settings
+import requests
 
+import arrow
+import json
 from soteria.configuration import Configuration
+from user_auth_token import UserTokenStore
+
 
 from app.agents.base import ApiMiner
 from app.agents.exceptions import (
@@ -9,9 +14,14 @@ from app.agents.exceptions import (
     LoginError,
 )
 from app.agents.schemas import Balance, Transaction
+from app.reporting import get_logger
 
+log = get_logger('squaremeal-restaurant-rewards')
 
 class SquaremealBase(ApiMiner):
+    token_store = UserTokenStore(settings.REDIS_URL)
+    AUTH_TOKEN_TIMEOUT = 3599
+
     def __init__(self, retry_count, user_info, scheme_slug=None):
         config = Configuration(
             scheme_slug,
@@ -21,12 +31,55 @@ class SquaremealBase(ApiMiner):
             settings.CONFIG_SERVICE_URL,
         )
         self.base_url = config.merchant_url
-        self.auth = config.security_credentials["outbound"]["credentials"][0]["value"]["url"]
+        self.auth_url = config.security_credentials["outbound"]["credentials"][0]["value"]["url"]
         self.secondary_key = str(config.security_credentials["outbound"]["credentials"][0]["value"]["secondary-key"])
         self.channel = user_info.get("channel", "Bink")
         self.point_transactions = []
         super().__init__(retry_count, user_info, scheme_slug=scheme_slug)
-        self.headers = {"Authorization":"", "Secondary-Key": self.secondary_key}
+
+    def authenticate(self):
+        have_valid_token = False
+        current_timestamp = arrow.utcnow().int_timestamp,
+        token = {}
+        try:
+            token = json.loads(self.token_store.get(self.scheme_id))
+            try:
+                if self._token_is_valid(token, current_timestamp):
+                    have_valid_token = True
+            except (KeyError, TypeError) as ex:
+                log.exception(ex)
+        except(KeyError, self.token_store.NoSuchToken):
+            pass
+
+        if not have_valid_token:
+            sm_access_token = self._refresh_token()
+            token = self._store_token(sm_access_token, current_timestamp)
+
+        return token["sm_access_token"]
+
+    def _refresh_token(self):
+        url = self.auth_url
+        payload = {
+            "grant_type": "client_credentials",
+            "client_secret": settings.AZURE_SM_CLIENT_SECRET,
+            "client_id": settings.AZURE_SM_CLIENT_ID,
+            "scope": settings.AZURE_SM_SCOPE
+        }
+        resp = requests.post(url, data=payload)
+        token = resp.json()["access_token"]
+        return token
+
+    def _store_token(self, token, current_timestamp):
+        token = {
+            "sm_access_token": token,
+            "timestamp": current_timestamp,
+        }
+        self.token_store.set(scheme_account_id=self.scheme_id, token=json.dumps(token))
+        return token
+
+    def _token_is_valid(self, token, current_timestamp):
+        time_diff = current_timestamp[0] - token["timestamp"][0]
+        return time_diff < self.AUTH_TOKEN_TIMEOUT
 
 
 class Squaremeal(SquaremealBase):
@@ -36,6 +89,7 @@ class Squaremeal(SquaremealBase):
     def join(self, credentials):
         consents = credentials.get("consents", [])
         url = f"{self.base_url}register"
+        self.headers = {"Authorization": f"Bearer {self.authenticate()}", "Secondary-Key": self.secondary_key}
         payload = {
             "email": credentials["email"],
             "password": credentials["password"],
@@ -67,6 +121,7 @@ class Squaremeal(SquaremealBase):
             return
 
         url = f"{self.base_url}login"
+        self.headers = {"Authorization": f"Bearer {self.authenticate()}", "Secondary-Key": self.secondary_key}
         payload = {
             "email": credentials["email"],
             "password": credentials["password"]
@@ -97,6 +152,7 @@ class Squaremeal(SquaremealBase):
         credentials = self.user_info["credentials"]
         merchant_id = credentials["merchant_identifier"]
         url = f"{self.base_url}points/{merchant_id}"
+        self.headers = {"Authorization": f"Bearer {self.authenticate()}", "Secondary-Key": self.secondary_key}
         resp = self.make_request(url, method="get")
 
         points_data = resp.json()
