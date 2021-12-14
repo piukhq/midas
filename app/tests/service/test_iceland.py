0b6eddb786ea404d27990676afcc5adb31ee622b
carla <cgouws@bink.com>
2021-12-14 19:30:53 +0000

    all tests for iceland

--- app/tests/service/test_iceland.py
+++ app/tests/service/test_iceland.py
@@ -1,13 +1,15 @@
-from unittest import TestCase, mock, main
-from unittest.mock import ANY, MagicMock, call
+import json
+from decimal import Decimal
+from unittest import TestCase, main, mock
+from unittest.mock import MagicMock
 
 import httpretty
-import json
 
-import app.agents.iceland
+from app.agents.base import Balance
 from app.agents.exceptions import LoginError
-from app.agents.merchant_api_generic import MerchantAPIGeneric
 from app.agents.iceland import Iceland
+from app.agents.merchant_api_generic import MerchantAPIGeneric
+from app.scheme_account import TWO_PLACES
 from app.tests.service.logins import AGENT_CLASS_ARGUMENTS, AGENT_CLASS_ARGUMENTS_FOR_VALIDATE
 
 cred = {
@@ -15,22 +17,49 @@ cred = {
     "password": "testpassword",
 }
 
-example_response_200 = {
-    "card_number": "6332040030541927281",
-    "barcode": "633204003054192728100088",
-    "balance": 0.0,
-    "unit": "GBP",
-    "alt_balance": 0.0,
-    "message_uid": "cd5ba192-8445-4d3f-9eff-cec90fff4185",
-    "record_uid": "pym1834v0zrqxnrmdod6jdglepko5972",
-    "merchant_scheme_id1": "ygdxz4y73lko5nvj500npr1jm9082vqe",
-    "merchant_scheme_id2": "46887678"
-}
-
 
 # Needs to be renamed to TestIceland once it has replaced the existing class TestIceland
 class TestIcelandTemp(TestCase):
-    @mock.patch("app.agents.iceland.Iceland._get_oauth_token", return_value="12345")
+    @httpretty.activate
+    @mock.patch("app.agents.iceland.Configuration")
+    def test_get_oauth_token(self, mock_configuration):
+        mock_configuration_object = MagicMock()
+        mock_configuration_object.security_credentials = {
+            "inbound": {"service": 1, "credentials": []},
+            "outbound": {
+                "service": 2,
+                "credentials": [
+                    {
+                        "credential_type": "compound_key",
+                        "storage_key": "a_storage_key",
+                        "value": {
+                            "payload": {
+                                "client_id": "a_client_id",
+                                "client_secret": "a_client_secret",
+                                "grant_type": "client_credentials",
+                                "resource": "a_resource",
+                            },
+                            "prefix": "Bearer",
+                            "url": "https://reflector.dev.gb.bink.com/mock/api/v1/bink/link",
+                        },
+                    }
+                ],
+            },
+        }
+        mock_configuration.return_value = mock_configuration_object
+
+        httpretty.register_uri(
+            httpretty.POST,
+            "https://reflector.dev.gb.bink.com/mock/api/v1/bink/link",
+            responses=[httpretty.Response(body=json.dumps({"access_token": "a_token"}), status=200)],
+        )
+
+        agent = Iceland(*AGENT_CLASS_ARGUMENTS_FOR_VALIDATE, scheme_slug="iceland-bonus-card-temp")
+
+        self.assertEqual(agent._get_oauth_token(), "a_token")
+
+    @httpretty.activate
+    @mock.patch("app.agents.iceland.Iceland._get_oauth_token", return_value="a_token")
     @mock.patch("app.agents.iceland.Configuration")
     def test_login_200(self, mock_configuration, mock_oath):
         merchant_url = "https://customergateway-uat.iceland.co.uk/api/v1/bink/link"
@@ -38,58 +67,72 @@ class TestIcelandTemp(TestCase):
 
         mock_configuration_object = MagicMock()
         mock_configuration_object.merchant_url = merchant_url
-        mock_configuration_object.handler_type = (2, "VALIDATE")
+        mock_configuration_object.callback_url = None
         mock_configuration.return_value = mock_configuration_object
 
         httpretty.register_uri(
             method=httpretty.POST,
             uri=merchant_url,
-            responses=[httpretty.Response(body=json.dumps({}), status=200)],
+            responses=[httpretty.Response(body=json.dumps({"balance": 10.0}), status=200)],
         )
 
         agent = Iceland(*AGENT_CLASS_ARGUMENTS_FOR_VALIDATE, scheme_slug="iceland-bonus-card-temp")
 
         agent.login(credentials)
+        self.assertEqual(agent.headers, {"Authorization": f"Bearer {'a_token'}"})
+        self.assertEqual(agent._balance_amount, 10.0)
+
+    @mock.patch("app.agents.iceland.Configuration")
+    def test_balance(self, mock_configuration):
+        agent = Iceland(*AGENT_CLASS_ARGUMENTS_FOR_VALIDATE, scheme_slug="iceland-bonus-card-temp")
+        agent._balance_amount = amount = Decimal(10.0).quantize(TWO_PLACES)
+        expected_result = Balance(
+            points=amount,
+            value=amount,
+            value_label="£{}".format(amount),
+        )
+
+        self.assertEqual(agent.balance(), expected_result)
 
 
-# class TestIceland(TestCase):
-#     @classmethod
-#     def setUpClass(cls):
-#         cls.i = MerchantAPIGeneric(*AGENT_CLASS_ARGUMENTS, scheme_slug="iceland-bonus-card")
-#         cls.i.attempt_login(cred)
-#
-#     def test_fetch_balance(self):
-#         balance = self.i.balance()
-#         self.assertIsNotNone(balance)
-#
-#     def test_transactions(self):
-#         transactions = self.i.transactions()
-#         self.assertIsNotNone(transactions)
-#
-#
-# class TestIcelandValidate(TestCase):
-#     @classmethod
-#     def setUpClass(cls):
-#         cls.i = MerchantAPIGeneric(*AGENT_CLASS_ARGUMENTS_FOR_VALIDATE, scheme_slug="iceland-bonus-card")
-#         credentials = cred
-#         credentials.pop("merchant_identifier", None)
-#
-#         cls.i.attempt_login(cred)
-#
-#     def test_validate(self):
-#         balance = self.i.balance()
-#         self.assertIsNotNone(balance)
-#
-#
-# class TestIcelandFail(TestCase):
-#     def test_login_fail(self):
-#         i = MerchantAPIGeneric(*AGENT_CLASS_ARGUMENTS_FOR_VALIDATE, scheme_slug="iceland-bonus-card")
-#         credentials = cred
-#         credentials["last_name"] = "midastest"
-#         credentials.pop("merchant_identifier", None)
-#         with self.assertRaises(LoginError) as e:
-#             i.attempt_login(credentials)
-#         self.assertEqual(e.exception.name, "Invalid credentials")
+class TestIceland(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.i = MerchantAPIGeneric(*AGENT_CLASS_ARGUMENTS, scheme_slug="iceland-bonus-card")
+        cls.i.attempt_login(cred)
+
+    def test_fetch_balance(self):
+        balance = self.i.balance()
+        self.assertIsNotNone(balance)
+
+    def test_transactions(self):
+        transactions = self.i.transactions()
+        self.assertIsNotNone(transactions)
+
+
+class TestIcelandValidate(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.i = MerchantAPIGeneric(*AGENT_CLASS_ARGUMENTS_FOR_VALIDATE, scheme_slug="iceland-bonus-card")
+        credentials = cred
+        credentials.pop("merchant_identifier", None)
+
+        cls.i.attempt_login(cred)
+
+    def test_validate(self):
+        balance = self.i.balance()
+        self.assertIsNotNone(balance)
+
+
+class TestIcelandFail(TestCase):
+    def test_login_fail(self):
+        i = MerchantAPIGeneric(*AGENT_CLASS_ARGUMENTS_FOR_VALIDATE, scheme_slug="iceland-bonus-card")
+        credentials = cred
+        credentials["last_name"] = "midastest"
+        credentials.pop("merchant_identifier", None)
+        with self.assertRaises(LoginError) as e:
+            i.attempt_login(credentials)
+        self.assertEqual(e.exception.name, "Invalid credentials")
 
 
 if __name__ == "__main__":
