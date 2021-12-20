2ee766e790bb2df5499c12173f06947027797c78
carla <cgouws@bink.com>
2021-12-20 16:50:30 +0000

    Add environment variable to control which Iceland agent is activated

--- app/active.py
+++ app/active.py
@@ -2,9 +2,10 @@
 
 # These could be strings, if we end up with too many imports
 # Commented entries are agents whose tests are currently not passing.
+from settings import NEW_ICELAND_AGENT_ACTIVE
+
 AGENTS = {
-    "iceland-bonus-card": "iceland_merchant_integration.Iceland",
-    "iceland-bonus-card-temp": "iceland.Iceland",
+    "iceland-bonus-card": "iceland.Iceland" if NEW_ICELAND_AGENT_ACTIVE else "iceland_merchant_integration.Iceland",
     "iceland-bonus-card-mock": "mock_agents.MockAgentIce",
     "harvey-nichols-mock": "mock_agents.MockAgentHN",
     "harvey-nichols": "harvey_nichols.HarveyNichols",
