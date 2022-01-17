1d921376a18889a018311cab1c089fdbacf987d2
Michal Jozwiak <mjozwiak@bink.com>
2022-01-17 13:11:09 +0000

    update signals outside of make_requests

--- app/agents/base.py
+++ app/agents/base.py
@@ -595,6 +595,7 @@ class MerchantApi(BaseMiner):
             handler_type=self.config.handler_type[0],
             integration_service=self.config.integration_service,
             status_code=0,  # Doesn't have a status code since this is an async response
+            channel=self.channel
         )
 
         if self._check_for_error_response(self.result):
@@ -689,6 +690,7 @@ class MerchantApi(BaseMiner):
             scheme_slug=self.config.scheme_slug,
             handler_type=self.config.handler_type[0],
             integration_service=self.config.integration_service,
+            channel=self.channel
         )
 
         response = requests.post(f"{self.config.merchant_url}", **self.request)
@@ -712,6 +714,7 @@ class MerchantApi(BaseMiner):
             handler_type=self.config.handler_type[0],
             integration_service=self.config.integration_service,
             status_code=status,
+            channel=self.channel
         )
 
         # Send signal for fail if not 2XX response
