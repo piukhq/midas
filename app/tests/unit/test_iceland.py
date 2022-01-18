5ab80cf764a7154c7bf9a97304424931701852b6
carla <cgouws@bink.com>
2022-01-18 10:18:00 +0000

    linting

--- app/tests/unit/test_iceland.py
+++ app/tests/unit/test_iceland.py
@@ -4,7 +4,6 @@ from http import HTTPStatus
 from unittest import TestCase, mock
 from unittest.mock import ANY, MagicMock, call
 
-import app.agents.iceland
 import arrow
 import httpretty
 from soteria.configuration import Configuration
