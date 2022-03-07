fe154b3a6497c33f9306e0d830c7158104c29e25
Michal Jozwiak <mjozwiak@bink.com>
2022-03-07 12:21:52 +0000

    vouchers refactor

--- app/vouchers.py
+++ app/vouchers.py
@@ -1,4 +1,31 @@
+import enum
+
 import arrow
+
+
+@enum.unique
+class VoucherType(enum.Enum):
+    JOIN = 0
+    ACCUMULATOR = 1
+    STAMPS = 2
+
+
+@enum.unique
+class VoucherState(enum.Enum):
+    ISSUED = 0
+    IN_PROGRESS = 1
+    EXPIRED = 2
+    REDEEMED = 3
+    CANCELLED = 4
+
+
+voucher_state_names = {
+    VoucherState.ISSUED: "issued",
+    VoucherState.IN_PROGRESS: "inprogress",
+    VoucherState.EXPIRED: "expired",
+    VoucherState.REDEEMED: "redeemed",
+    VoucherState.CANCELLED: "cancelled",
+}
 
 
 def generate_pending_voucher_code(timestamp):
