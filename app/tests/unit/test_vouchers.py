a3f0e95a3d2daf80e2b7c32b55a8125b29e8e9cb
Michal Jozwiak <mjozwiak@bink.com>
2022-03-07 10:26:36 +0000

    refactor vouchers

---
+++ app/tests/unit/test_vouchers.py
@@ -0,0 +1,9 @@
+import unittest
+
+from app.vouchers import generate_pending_voucher_code
+
+
+class TestVouchers(unittest.TestCase):
+    def test_generate_pending_voucher_code(self):
+        self.assertEqual(generate_pending_voucher_code(1647512390), "Due17thMar 2022")
+        self.assertEqual(generate_pending_voucher_code(1646648390), "Due 7thMar 2022")
--- app/vouchers.py
+++ app/vouchers.py
@@ -1,46 +1,4 @@
-import enum
-
-import arrow
 import pendulum
-
-
-@enum.unique
-class VoucherType(enum.Enum):
-    JOIN = 0
-    ACCUMULATOR = 1
-    STAMPS = 2
-
-
-@enum.unique
-class VoucherState(enum.Enum):
-    ISSUED = 0
-    IN_PROGRESS = 1
-    EXPIRED = 2
-    REDEEMED = 3
-    CANCELLED = 4
-
-
-voucher_state_names = {
-    VoucherState.ISSUED: "issued",
-    VoucherState.IN_PROGRESS: "inprogress",
-    VoucherState.EXPIRED: "expired",
-    VoucherState.REDEEMED: "redeemed",
-    VoucherState.CANCELLED: "cancelled",
-}
-
-
-def get_voucher_state(issue_date, redeem_date, expiry_date):
-    if redeem_date is not None:
-        state = VoucherState.REDEEMED
-    elif issue_date is not None:
-        if expiry_date <= arrow.utcnow():
-            state = VoucherState.EXPIRED
-        else:
-            state = VoucherState.ISSUED
-    else:
-        state = VoucherState.IN_PROGRESS
-
-    return state
 
 
 def generate_pending_voucher_code(timestamp):
