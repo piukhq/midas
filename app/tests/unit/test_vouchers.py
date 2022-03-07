504c72183755393a6a9c391aef386b33709d27ac
Michal Jozwiak <mjozwiak@bink.com>
2022-03-07 15:37:57 +0000

    update vouchers tests

--- app/tests/unit/test_vouchers.py
+++ app/tests/unit/test_vouchers.py
@@ -5,5 +5,5 @@ from app.vouchers import generate_pending_voucher_code
 
 class TestVouchers(unittest.TestCase):
     def test_generate_pending_voucher_code(self):
-        self.assertEqual(generate_pending_voucher_code(1647512390), "Due:17thMar 2022")
-        self.assertEqual(generate_pending_voucher_code(1646648390), "Due: 7thMar 2022")
+        self.assertEqual("Due:17thMar 2022", generate_pending_voucher_code(1647512390))
+        self.assertEqual("Due: 7thMar 2022", generate_pending_voucher_code(1646648390))
