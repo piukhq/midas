58d31d4c3e62a4687e521b1eecb93dff7ad8f255
Michal Jozwiak <mjozwiak@bink.com>
2022-03-15 12:27:20 +0000

    modify tests

--- app/tests/unit/test_requests_retry.py
+++ app/tests/unit/test_requests_retry.py
@@ -1,16 +1,30 @@
+import httpretty
 import pytest
-from requests.exceptions import ConnectionError
+from requests.exceptions import RetryError
 
 from app.requests_retry import requests_retry_session
 
 
+@httpretty.activate
 def test_retry_session():
+    httpretty.register_uri(
+        method=httpretty.GET,
+        uri="https://httpbin.org/get",
+        responses=[httpretty.Response(body="", status=200)],
+    )
     s = requests_retry_session()
     resp = s.get("https://httpbin.org/get")
     assert resp.status_code == 200
 
 
-def test_retry_delayed():
-    s = requests_retry_session(retries=0)
-    with pytest.raises(ConnectionError):
-        s.get("https://httpbin.org/delay/30", timeout=1)
+@httpretty.activate
+def test_retries_on_error():
+    httpretty.register_uri(
+        method=httpretty.GET,
+        uri="https://httpbin.org/get",
+        responses=[httpretty.Response(body="", status=502)],
+    )
+    s = requests_retry_session(retries=3)
+    with pytest.raises(RetryError):
+        s.get("https://httpbin.org/get")
+    assert len(httpretty.latest_requests()) == 4
