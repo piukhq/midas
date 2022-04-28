153160782aa5047f533cf977bbb223061ccb86df
carla <cgouws@bink.com>
2022-04-28 16:40:15 +0100

    Exception classes take 2

--- app/agents/iceland.py
+++ app/agents/iceland.py
@@ -6,6 +6,7 @@ import arrow
 from blinker import signal
 from soteria.configuration import Configuration
 
+import settings
 from app import publish
 from app.agents.base import JOURNEY_TYPE_TO_HANDLER_TYPE_MAPPING, Balance, BaseAgent, check_correct_authentication
 from app.agents.exceptions import (
@@ -22,18 +23,26 @@ from app.agents.exceptions import (
     STATUS_LOGIN_FAILED,
     UNKNOWN,
     AgentError,
-    JoinError,
+    JoinError as JoinErrorOld,
     LoginError,
 )
 from app.agents.schemas import Transaction
 from app.encryption import hash_ids
 from app.exceptions import AgentException
 from app.reporting import get_logger
-from app.scheme_account import TWO_PLACES, SchemeAccountStatus, update_pending_join_account
+from app.scheme_account import TWO_PLACES, JourneyTypes, SchemeAccountStatus, update_pending_join_account
 from app.tasks.resend_consents import ConsentStatus
 
 RETRY_LIMIT = 3
 log = get_logger("iceland")
+
+
+JOURNEY_TYPE_TO_HANDLER_TYPE_MAPPING = {
+    JourneyTypes.JOIN: Configuration.JOIN_HANDLER,
+    JourneyTypes.LINK: Configuration.VALIDATE_HANDLER,
+    JourneyTypes.ADD: Configuration.VALIDATE_HANDLER,
+    JourneyTypes.UPDATE: Configuration.UPDATE_HANDLER,
+}
 
 
 class Iceland(BaseAgent):
@@ -46,40 +55,47 @@ class Iceland(BaseAgent):
             config=config,
         )
         self.source_id = "iceland"
-        self.oauth_token_timeout = 3599
-        self.outbound_security_credentials = self.config.security_credentials["outbound"]["credentials"][0]["value"]
-        self.outbound_auth_service = self.config.security_credentials["outbound"]["service"]
-        self.credentials = self.user_info["credentials"]
+        handler_type = JOURNEY_TYPE_TO_HANDLER_TYPE_MAPPING[user_info["journey_type"]]
+        self.config = config or Configuration(
+            scheme_slug,
+            handler_type,
+            settings.VAULT_URL,
+            settings.VAULT_TOKEN,
+            settings.CONFIG_SERVICE_URL,
+        )
+        self.auth_token_timeout = 3599
+        self.security_credentials = self.config.security_credentials["outbound"]["credentials"][0]["value"]
+        self.authentication_service = self.config.security_credentials["outbound"]["service"]
         self._balance_amount = None
         self._transactions = None
         self.errors = {
-            ACCOUNT_ALREADY_EXISTS: ["ALREADY_PROCESSED", "ACCOUNT_ALREADY_EXISTS"],
-            CARD_NOT_REGISTERED: "CARD_NOT_REGISTERED",
-            CARD_NUMBER_ERROR: "CARD_NUMBER_ERROR",
-            GENERAL_ERROR: "GENERAL_ERROR",
-            JOIN_ERROR: "JOIN_ERROR",
-            JOIN_IN_PROGRESS: "JOIN_IN_PROGRESS",
-            LINK_LIMIT_EXCEEDED: "LINK_LIMIT_EXCEEDED",
-            NO_SUCH_RECORD: "NO_SUCH_RECORD",
-            NOT_SENT: "NOT_SENT",
-            PRE_REGISTERED_CARD: "PRE_REGISTERED_ERROR",
-            STATUS_LOGIN_FAILED: "VALIDATION",
-            UNKNOWN: "UNKNOWN",
+            AccountAlreadyExistsError: ["ALREADY_PROCESSED", "ACCOUNT_ALREADY_EXISTS"],
+            CardNotRegisteredError: "CARD_NOT_REGISTERED",
+            CardNumberError: "CARD_NUMBER_ERROR",
+            GeneralError: "GENERAL_ERROR",
+            JoinError: "JOIN_ERROR",
+            JoinInProgressError: "JOIN_IN_PROGRESS",
+            LinkLimitExceededError: "LINK_LIMIT_EXCEEDED",
+            NoSuchRecordError: "NO_SUCH_RECORD",
+            NotSentError: "NOT_SENT",
+            PreRegisteredCardError: "PRE_REGISTERED_ERROR",
+            StatusLoginFailedError: "VALIDATION",
+            UnknownError: "UNKNOWN",
         }
 
     def get_auth_url_and_payload(self):
-        url = self.outbound_security_credentials["url"]
+        url = self.security_credentials["url"]
         payload = {
             "grant_type": "client_credentials",
-            "client_secret": self.outbound_security_credentials["payload"]["client_secret"],
-            "client_id": self.outbound_security_credentials["payload"]["client_id"],
-            "resource": self.outbound_security_credentials["payload"]["resource"],
+            "client_secret": self.security_credentials["payload"]["client_secret"],
+            "client_id": self.security_credentials["payload"]["client_id"],
+            "resource": self.security_credentials["payload"]["resource"],
         }
         return url, payload
 
     def _add_additional_consent(self) -> None:
-        if len(self.credentials["consents"]) < 2:
-            journey_type = self.credentials["consents"][0]["journey_type"]
+        if len(self.user_info["credentials"]["consents"]) < 2:
+            journey_type = self.user_info["credentials"]["consents"][0]["journey_type"]
             consent = {
                 "id": 99999999999,
                 "slug": "marketing_opt_in_thirdparty",
@@ -87,22 +103,23 @@ class Iceland(BaseAgent):
                 "created_on": arrow.now().isoformat(),  # '2020-05-26T15:30:16.096802+00:00',
                 "journey_type": journey_type,
             }
-            self.credentials["consents"].append(consent)
+            self.user_info["credentials"]["consents"].append(consent)
         else:
             log.debug("Too many consents for Iceland scheme.")
 
     def _create_join_request_payload(self) -> dict:
-        marketing_mapping = {i["slug"]: i["value"] for i in self.credentials["consents"]}
+        credentials = self.user_info["credentials"]
+        marketing_mapping = {i["slug"]: i["value"] for i in credentials["consents"]}
         return {
-            "town_city": self.credentials["town_city"],
-            "county": self.credentials["county"],
-            "title": self.credentials["title"],
-            "address_1": self.credentials["address_1"],
-            "first_name": self.credentials["first_name"],
-            "last_name": self.credentials["last_name"],
-            "email": self.credentials["email"],
-            "postcode": self.credentials["postcode"],
-            "address_2": self.credentials["address_2"],
+            "town_city": credentials["town_city"],
+            "county": credentials["county"],
+            "title": credentials["title"],
+            "address_1": credentials["address_1"],
+            "first_name": credentials["first_name"],
+            "last_name": credentials["last_name"],
+            "email": credentials["email"],
+            "postcode": credentials["postcode"],
+            "address_2": credentials["address_2"],
             "record_uid": self.record_uid,
             "country": self.config.country,
             "message_uid": self.message_uid,
@@ -110,8 +127,8 @@ class Iceland(BaseAgent):
             "marketing_opt_in": marketing_mapping.get("marketing_opt_in"),
             "marketing_opt_in_thirdparty": marketing_mapping.get("marketing_opt_in_thirdparty"),
             "merchant_scheme_id1": hash_ids.encode(sorted(map(int, self.user_info["user_set"].split(",")))[0]),
-            "dob": self.credentials["date_of_birth"],
-            "phone1": self.credentials["phone"],
+            "dob": credentials["date_of_birth"],
+            "phone1": credentials["phone"],
         }
 
     def _process_join_callback_response(self, data):
@@ -129,20 +146,20 @@ class Iceland(BaseAgent):
         try:
             error = data.get("error_codes")
             if error:
-                self.handle_errors(error_code=error[0]["code"], exception_type=JoinError)
+                self.handle_errors(error_code=error[0]["code"], exception_type=JoinErrorOld)
             update_pending_join_account(self.user_info, "success", self.message_uid, identifier=self.identifier)
             consent_status = ConsentStatus.SUCCESS
-        except (AgentException, LoginError, JoinError, AgentError):
+        except (AgentException, LoginError, JoinErrorOld, AgentError):
             consent_status = ConsentStatus.FAILED
             raise
         finally:
-            self.consent_confirmation(self.credentials.get("consents", []), consent_status)
+            self.consent_confirmation(self.user_info["credentials"].get("consents", []), consent_status)
 
         status = SchemeAccountStatus.ACTIVE
         publish.status(self.scheme_id, status, self.message_uid, self.user_info, journey="join")
 
     def join_callback(self, data: dict) -> None:
-        self.integration_service = "SYNC"
+        self.integration_service = Configuration.INTEGRATION_CHOICES[Configuration.SYNC_INTEGRATION][1].upper()
         self.identifier = {
             "barcode": data.get("barcode"),  # type:ignore
             "card_number": data.get("card_number"),  # type:ignore
@@ -166,22 +183,22 @@ class Iceland(BaseAgent):
             if response.text == "":
                 return {}
             return response.json()
-        except (JoinError, AgentError) as e:
+        except (JoinErrorOld, AgentError) as e:
             signal("join-fail").send(self, slug=self.scheme_slug, channel=self.channel)
             self.handle_errors(e.name)
 
-    def join(self) -> None:
+    def join(self, data: dict) -> None:
         # Barclays expects only 1 consent for an Iceland join, whereas Iceland expects 2
         # Save the current consent to a variable for use in self.consent_confirmation below
-        consents = self.credentials.get("consents", []).copy()
-        self.integration_service = "ASYNC"
+        consents = self.user_info["credentials"].get("consents", []).copy()
+        self.integration_service = Configuration.INTEGRATION_CHOICES[Configuration.ASYNC_INTEGRATION][1].upper()
         self.expecting_callback = True
         # Add the additional consent for Iceland
         if consents:
             self._add_additional_consent()
 
         check_correct_authentication(
-            actual_config_auth_type=self.outbound_auth_service,
+            actual_config_auth_type=self.authentication_service,
             allowed_config_auth_types=[Configuration.OPEN_AUTH_SECURITY, Configuration.OAUTH_SECURITY],
         )
         self.authenticate()
@@ -192,7 +209,7 @@ class Iceland(BaseAgent):
         if error:
             consent_status = ConsentStatus.FAILED
             self.consent_confirmation(consents, consent_status)
-            self.handle_errors(error[0]["code"], exception_type=JoinError)
+            self.handle_errors(error[0]["code"], exception_type=JoinErrorOld)
 
         consent_status = ConsentStatus.PENDING
         self.consent_confirmation(consents, consent_status)
@@ -205,24 +222,24 @@ class Iceland(BaseAgent):
             signal("log-in-fail").send(self, slug=self.scheme_slug)
             self.handle_errors(e.name)
 
-    def login(self) -> None:
-        self.integration_service = "SYNC"
+    def login(self, credentials) -> None:
+        self.integration_service = Configuration.INTEGRATION_CHOICES[Configuration.SYNC_INTEGRATION][1].upper()
 
         check_correct_authentication(
-            actual_config_auth_type=self.outbound_auth_service,
+            actual_config_auth_type=self.authentication_service,
             allowed_config_auth_types=[Configuration.OPEN_AUTH_SECURITY, Configuration.OAUTH_SECURITY],
         )
         self.authenticate()
 
         payload = {
-            "card_number": self.credentials["card_number"],
-            "last_name": self.credentials["last_name"],
-            "postcode": self.credentials["postcode"],
+            "card_number": credentials["card_number"],
+            "last_name": credentials["last_name"],
+            "postcode": credentials["postcode"],
             "message_uid": self.message_uid,
             "record_uid": self.record_uid,
             "callback_url": self.config.callback_url,
             "merchant_scheme_id1": hash_ids.encode(sorted(map(int, self.user_info["user_set"].split(",")))[0]),
-            "merchant_scheme_id2": self.credentials.get("merchant_identifier"),
+            "merchant_scheme_id2": credentials.get("merchant_identifier"),
         }
 
         response_json = self._login(payload)
@@ -240,7 +257,7 @@ class Iceland(BaseAgent):
                 "card_number": response_json.get("card_number"),
                 "merchant_identifier": response_json.get("merchant_scheme_id2"),
             }
-        self.credentials.update(self.identifier)
+        self.user_info["credentials"].update(self.identifier)
 
         self._balance_amount = response_json["balance"]
         self._transactions = response_json.get("transactions")
--- app/exceptions.py
+++ app/exceptions.py
@@ -1,3 +1,16 @@
+"""
+Error code notes:
+
+https://developer.yodlee.com/FAQs/Error_Codes
+https://en.wikipedia.org/wiki/List_of_HTTP_codes
+http://www.iana.org/assignments/http-status-codes/http-status-codes.xhtml
+
+4xx client errors, custom error codes are in the range 432-440
+5xx service errors, custom error codes are in the range 530-540
+"""
+import sentry_sdk
+
+
 class AgentException(Exception):
     def __init__(self, error):
         self.status_code = return_error_code(error)
@@ -14,3 +27,247 @@ def return_error_code(exception):
     except AttributeError:
         # Return unknown error status code
         return 520
+
+
+class BaseError(Exception):
+    """Exception raised for errors in the input."""
+
+    def __init__(self):
+        self.name: str = ""
+        self.message: str = ""
+        self.code: int = None
+        self.response = None
+        self.system_action_required = False
+
+    def __str__(self):
+        return f"{self.code} {self.__repr__()}: {self.name}: {self.message}"
+
+
+class ValidationError(BaseError):
+    def __init__(self, response=None):
+        self.code = 401
+        self.name = "Failed validation"
+        self.message = "Validation of the request has failed."
+
+
+class StatusLoginFailedError(BaseError):
+    def __init__(self, response=None):
+        self.code = 403
+        self.name = "Invalid credentials"
+        self.message = """We could not update your account because your username and/or password 
+        were reported to be incorrect. Please re-verify your username and password."""
+        self.response = response
+
+
+class PreRegisteredCardError(BaseError):
+    def __init__(self, response=None):
+        self.code = 406
+        self.name = "Pre-registered card"
+        self.message = """We could not link your account because this card does not exist yet in 
+        this loyalty scheme. Please join this loyalty scheme with those credentials and try again."""
+        self.response = response
+
+
+class RetryLimitReachedError(BaseError):
+    def __init__(self, response=None):
+        self.code = 429
+        self.name = "Retry limit reached"
+        self.message = "You have reached your maximum amount of login tries. Please wait 15 minutes."
+        self.system_action_required = True
+
+
+class StatusAccountLockedError(BaseError):
+    def __init__(self, response=None):
+        self.code = 434
+        self.name = "Account locked on end site"
+        self.message = """We could not update your account because it appears your account has been 
+        locked. This usually results from too many unsuccessful login attempts in a short period of 
+        time. Please visit the site or contact its customer support to resolve this issue. Once done, 
+        please update your account credentials in case they are changed."""
+
+
+class WrongCredentialTypeError(BaseError):
+    def __init__(self, response=None):
+        self.code = 435
+        self.name = "Wrong credential type entered"
+        self.message = """One of the account credentials you have entered is the wrong type. 
+        For example, you may have entered your card number instead of your barcode. Please 
+        correct this information and try again."""
+
+
+class CardNumberError(BaseError):
+    def __init__(self, response=None):
+        self.code = 438
+        self.name = "Card not registered or Unknown"
+        self.message = "Unknown Card number."
+        self.response = response
+
+
+class LinkLimitExceededError(BaseError):
+    def __init__(self, response=None):
+        self.code = 438
+        self.name = "Card not registered or Unknown"
+        self.message = "Unknown Card number."
+        self.response = response
+
+
+class CardNotRegisteredError(BaseError):
+    def __init__(self, response=None):
+        self.code = 438
+        self.name = "Card not registered or Unknown"
+        self.message = "Unknown Card number."
+        self.response = response
+
+
+class GeneralError(BaseError):
+    def __init__(self, response=None):
+        self.code = 439
+        self.name = "General Error"
+        self.message = "General Error such as incorrect user details."
+        self.response = response
+
+
+class StatusRegistrationFailedError(BaseError):
+    def __init__(self, response=None):
+        self.code = 440
+        self.name = "Join in progress"
+        self.message = """The username and/or password you have entered were reported to be invalid. 
+        This may due to password validation - it's too short, it requires capital letters and numbers, etc."""
+
+
+class JoinInProgressError(BaseError):
+    def __init__(self, response=None):
+        self.code = 441
+        self.name = "Join in progress"
+        self.message = "Join in progress."
+        self.response = response
+
+
+class NoSuchRecordError(BaseError):
+    def __init__(self, response=None):
+        self.code = 444
+        self.name = "Account does not exist"
+        self.message = "There is currently no account with the credentials you have provided."
+        self.system_action_required = True
+        self.response = response
+
+
+class AccountAlreadyExistsError(BaseError):
+    def __init__(self, response=None):
+        self.code = 445
+        self.name = "Account already exists"
+        self.message = "An account with this username/email already exists."
+        self.response = response
+
+
+class SchemeRequestedDeleteError(BaseError):
+    def __init__(self, response=None):
+        self.code = 447
+        self.name = "Scheme requested account deletion"
+        self.message = "The scheme has requested this account should be deleted."
+
+
+class ResourceLimitReachedError(BaseError):
+    def __init__(self, response=None):
+        self.code = 503
+        self.name = "Resource limit reached"
+        self.message = "There are currently too many balance requests running. Please wait before trying again."
+        self.system_action_required = True
+
+
+class EndSiteDownError(BaseError):
+    def __init__(self, response=None):
+        self.code = 530
+        self.name = "End site down"
+        self.message = "The scheme end site is currently down."
+        self.system_action_required = True
+
+
+class IPBlockedError(BaseError):
+    def __init__(self, response=None):
+        self.code = 531
+        self.name = "IP blocked"
+        self.message = "The end site is currently blocking this ip address."
+        self.system_action_required = True
+
+
+class PasswordExpiredError(BaseError):
+    def __init__(self, response=None):
+        self.code = 533
+        self.name = "Password expired"
+        self.message = """We could not update your account because the end site requires that 
+        you reset your password. Please visit the site and resolve this issue before trying again."""
+
+
+class NotSentError(BaseError):
+    def __init__(self, response=None):
+        self.code = 535
+        self.name = "Message was not sent"
+        self.message = "Message was not sent."
+        self.system_action_required = True
+        self.response = response
+
+
+class ConfigurationError(BaseError):
+    def __init__(self, response=None):
+        self.code = 536
+        self.name = "Configuration error"
+        self.message = "There is an error with the configuration or it was not possible to retrieve."
+        self.system_action_required = True
+
+
+class ServiceConnectionError(BaseError):
+    def __init__(self, response=None):
+        self.code = 537
+        self.name = "Service connection error"
+        self.message = "There was in issue connecting to an external service."
+
+
+class JoinError(BaseError):
+    def __init__(self, response=None):
+        self.code = 538
+        self.name = "General Error preventing join"
+        self.message = "A system error occurred during join."
+        self.system_action_required = True
+        self.response = response
+
+
+class UnknownError(BaseError):
+    def __init__(self, response=None):
+        self.code = 520
+        self.name = "Invalid mfa"
+        self.message = "We have no idea what went wrong - the team is on it."
+        self.system_action_required = True
+        self.response = response
+
+
+# class InvalidMFAInfoError(AgentError):
+#     def __init__(self, response=None):
+#         self.code = 438
+#         self.name = "Invalid MFA"
+#         self.message = "We're sorry, the authentication information you provided is incorrect. Please try again."
+
+
+# class ConfirmationRequiredError(AgentError):
+#     def __init__(self, response=None):
+#         self.code = 534
+#         self.name = "Confirmation required"
+#         self.message = """The end-site requires that you confirm some information before we
+#         can continue. Please log into your account on the end-site and follow through any
+#         confirmation steps shown, then try again."""
+
+
+# class TrippedCaptchaError(AgentError):
+#     def __init__(self, response=None):
+#         self.code = 532
+#         self.name = "Tripped captcha"
+#         self.message = "The agent has tripped the scheme capture."
+#         self.system_action_required = True
+
+
+class AgentModifiedError(Exception):
+    pass
+
+
+class UnauthorisedError(Exception):
+    pass
