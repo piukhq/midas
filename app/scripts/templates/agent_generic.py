93024da11ccc425216e65ca9a37aa661182abc45
mmjjoozz <jozw.michal@gmail.com>
2022-05-03 11:13:52 +0100

    add script (#83)
    * add script
    
    * add script to update active.py
    
    * add script to update active.py
    
    * fix typehints
    
    * fix typehints
    
    * fix
    
    * isort
    
    Co-authored-by: Michal Jozwiak <mjozwiak@bink.com>

---
+++ app/scripts/templates/agent_generic.py
@@ -0,0 +1,39 @@
+from typing import Any, Optional
+
+from app.agents.base import BaseAgent
+from app.agents.schemas import Balance, Transaction
+from app.reporting import get_logger
+
+RETRY_LIMIT = 3
+log = get_logger("template_slug")
+
+
+class TemplateClassName(BaseAgent):
+    def __init__(self, retry_count, user_info, scheme_slug=None):
+        super().__init__(retry_count, user_info, scheme_slug=scheme_slug)
+        self.source_id = "template_identifier"
+        self.base_url = self.config.merchant_url
+        self.integration_service = "SYNC"
+        self.oauth_token_timeout = 3599
+        self.outbound_security_credentials = self.config.security_credentials["outbound"]["credentials"][0]["value"]
+        self.outbound_auth_service = self.config.security_credentials["outbound"]["service"]
+        self.credentials = self.user_info["credentials"]
+        self.errors = {}
+
+    def join(self) -> Any:
+        pass
+
+    def login(self) -> Any:
+        pass
+
+    def transactions(self) -> list[Transaction]:
+        pass
+
+    def transaction_history(self) -> list[Transaction]:
+        pass
+
+    def parse_transaction(self, transaction: dict) -> Transaction:
+        pass
+
+    def balance(self) -> Optional[Balance]:
+        pass
