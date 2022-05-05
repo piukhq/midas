69470fbc23846b2fcbc598b16d17e5d4260a427a
carla <cgouws@bink.com>
2022-05-05 14:11:09 +0100

    linting

--- app/tests/unit/test_mock_agents.py
+++ app/tests/unit/test_mock_agents.py
@@ -8,17 +8,13 @@ class TestMockAgentHN(TestCase):
     def setUp(self):
         self.retry_count = 0
         self.user_info = {
-            "credentials": {
-                "consents": [],
-                "email": 'user@testbink.com',
-                "password": "paSSword"
-            },
+            "credentials": {"consents": [], "email": "user@testbink.com", "password": "paSSword"},
             "journey_type": 1,
             "scheme_account_id": 123456,
             "status": 0,
-            "user_set": '40308'
+            "user_set": "40308",
         }
-        self.scheme_slug = 'harvey-nichols-mock'
+        self.scheme_slug = "harvey-nichols-mock"
 
     def test_get_agent_class(self):
         agent_class = get_agent_class(scheme_slug=self.scheme_slug)
@@ -28,5 +24,3 @@ class TestMockAgentHN(TestCase):
     def test_agent_instance_does_not_create_config(self, mock_config):
         MockAgentHN(self.retry_count, self.user_info, scheme_slug=self.scheme_slug)
         self.assertEqual(0, mock_config.call_count)
-
-
