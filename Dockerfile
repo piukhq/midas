ab64dae3a6bf3ba89bafea33182552812f276c7c
Mick <6615305+Mick-Cadac@users.noreply.github.com>
2022-05-13 15:44:07 +0100

    MER-1699 - Remove credential from gunicorn logs (#91)
    * MER-1699 - Remove credential from gunicorn logs
    
    * MER-1699 - ALso check the log args
    
    * MER-1699 - black formatting

--- Dockerfile
+++ Dockerfile
@@ -6,5 +6,5 @@ ADD . .
 RUN pipenv install --system --deploy --ignore-pipfile
 
 ENTRYPOINT [ "linkerd-await", "--" ]
-CMD [ "gunicorn", "--workers=2", "--threads=2", "--error-logfile=-", \
+CMD [ "gunicorn", "--workers=2", "--threads=2", "--error-logfile=-", "--logger-class=gunicorn_log_filter.Logger", \
     "--access-logfile=-", "--bind=0.0.0.0:9000", "wsgi:app" ]
---
+++ gunicorn_log_filter/__init__.py
@@ -0,0 +1,38 @@
+import logging
+import re
+
+from gunicorn import glogging
+
+
+class RedactingFilter(logging.Filter):
+    def __init__(self, patterns):
+        super(RedactingFilter, self).__init__()
+        self._patterns = patterns
+
+    def filter(self, record):
+        record.msg = self.redact(record.msg)
+        if isinstance(record.args, dict):
+            for k in record.args.keys():
+                record.args[k] = self.redact(record.args[k])
+        else:
+            record.args = tuple(self.redact(arg) for arg in record.args)
+        return True
+
+    def redact(self, msg):
+        if isinstance(msg, str):
+            for pattern in self._patterns:
+                msg = re.sub(pattern, "credentials=[REDACTED]", msg)
+        return msg
+
+
+class Logger(glogging.Logger):
+    """Custom logger for Gunicorn log messages."""
+
+    def setup(self, cfg):
+        super().setup(cfg)
+
+        patterns = [
+            re.compile(r"credentials=[a-zA-Z0-9+/=]*"),
+        ]
+        self.access_log.addFilter(RedactingFilter(patterns))
+        self.error_log.addFilter(RedactingFilter(patterns))
