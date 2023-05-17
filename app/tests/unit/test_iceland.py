d7aa9006312cace636b881f6ace2d13b3492d884
Michal Jozwiak <mjozwiak@bink.com>
2023-05-17 12:23:31 +0100

    black reformat iceland

--- app/tests/unit/test_iceland.py
+++ app/tests/unit/test_iceland.py
@@ -1135,7 +1135,7 @@ class TestIcelandJoin(TestCase):
                 integration_service="SYNC",
                 status_code=0,
                 channel="",
-                audit_config={}
+                audit_config={},
             ),
             call("callback-success"),
             call().send(
