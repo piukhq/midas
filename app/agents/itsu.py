5e9f567f46da02ea8dbfd57ce81da45ad2ba36ad
Michal Jozwiak <mjozwiak@bink.com>
2023-07-06 10:45:13 +0100

    store pepper_id instead of ctcid in hermes

--- app/agents/itsu.py
+++ app/agents/itsu.py
@@ -2,21 +2,18 @@ from http import HTTPStatus
 from typing import Optional
 from urllib.parse import urlencode, urljoin
 
-import arrow
 import sentry_sdk
 from _decimal import Decimal
 from blinker import signal
+from soteria.configuration import Configuration
 
 from app.agents.acteol import Acteol, log
-from app.agents.schemas import Balance, Voucher
+from app.agents.schemas import Balance
 from app.exceptions import BaseError, NoSuchRecordError
-from app.vouchers import VoucherState, voucher_state_names
 
 
 class Itsu(Acteol):
-    ORIGIN_ROOT = "Bink-Itsu"
     API_TIMEOUT = 10  # n_seconds until timeout for calls to Acteol's API
-    RETAILER_ID = "315"
     N_TRANSACTIONS = 5  # Number of transactions to return from Acteol's API
     # Number of attempts to send consents to Agent must be > 0
     # (0 = no send , 1 send once, 2 = 1 retry)
@@ -40,6 +37,13 @@ class Itsu(Acteol):
         payload = urlencode(payload)
         return url, payload
 
+    def authenticate(self):
+        if self.outbound_auth_service == Configuration.OPEN_AUTH_SECURITY:
+            return
+        if self.outbound_auth_service == Configuration.OAUTH_SECURITY:
+            self._oauth_authentication()
+        self.headers["Content-Type"] = "application/json"
+
     def _find_customer_details(self) -> list:
         self.authenticate()
         api_url = urljoin(self.base_url, "/api/Customer/FindCustomerDetails")
@@ -62,7 +66,7 @@ class Itsu(Acteol):
         payload = {"CtcID": ctcid, "SupInfo": [{"FieldName": "BinkActive", "FieldContent": "true"}]}
         resp = self.make_request(api_url, method="patch", timeout=self.API_TIMEOUT, json=payload)
         if resp.status_code != HTTPStatus.OK:
-            log.debug(f"Error while fetching customer details, reason: {resp.status_code} {resp.reason}")
+            log.debug(f"Error while patching customer details, reason: {resp.status_code} {resp.reason}")
             raise Exception
 
         self._check_response_for_error(resp.json())
@@ -83,9 +87,9 @@ class Itsu(Acteol):
                 # Set up attributes needed for the creation of an active membership card
                 self.identifier = {
                     "card_number": self.credentials["card_number"],
-                    "merchant_identifier": ctcid,
+                    "merchant_identifier": pepper_id,
                 }
-                self.credentials.update({"merchant_identifier": ctcid})
+                self.credentials.update({"merchant_identifier": pepper_id, "ctcid": ctcid})
             except BaseError:
                 signal("log-in-fail").send(self, slug=self.scheme_slug)
                 raise
@@ -104,10 +108,14 @@ class Itsu(Acteol):
     def balance(self) -> Optional[Balance]:
         # Ensure a valid API token
         self.authenticate()
-
         try:
+            ctcid = self.credentials.get("ctcid")
+            pepper_id = self.credentials.get("merchant_identifier")
+            if not ctcid:
+                # Find customer ctcid
+                ctcid, pepper_id = self._find_customer_details()
             # Get customer details
-            customer_details = self._get_customer_details(ctcid=self.credentials["merchant_identifier"])
+            customer_details = self._get_customer_details(ctcid=ctcid)
         except BaseError as ex:
             sentry_issue_id = sentry_sdk.capture_exception(ex)
             log.debug(
@@ -128,13 +136,7 @@ class Itsu(Acteol):
         self._check_deleted_user(resp_json=customer_details)
         points = Decimal(customer_details["LoyaltyPointsBalance"])
 
-        # Make sure we have a populated merchant_identifier in credentials. This is required to get voucher
-        # data from Acteol. Wasabi user’s credentials to be updated if they are updated within Acteol,
-        # so that the user’s scheme account reflects the correct data.
-        self.credentials["merchant_identifier"] = customer_details["CustomerID"]
-        ctcid = self.credentials["merchant_identifier"]
-
-        self.update_hermes_credentials(ctcid, customer_details)
+        self.update_hermes_credentials(pepper_id, customer_details)
 
         # Get all vouchers for this customer
         vouchers = self._get_vouchers(ctcid=ctcid)
@@ -147,6 +149,7 @@ class Itsu(Acteol):
 
         # Now create the other types of vouchers
         for voucher in vouchers:
+            voucher["VoucherCode"] = "----------"
             if voucher.get("Disabled"):  # Ignore cancelled vouchers
                 continue
             if bink_mapped_voucher := self._map_acteol_voucher_to_bink_struct(voucher=voucher):
@@ -158,46 +161,3 @@ class Itsu(Acteol):
             value_label="",
             vouchers=bink_mapped_vouchers,
         )
-
-    def _make_expired_voucher(self, voucher: dict, current_datetime: arrow.Arrow) -> Optional[Voucher]:
-        """
-        Make a Bink expired voucher dict if the Acteol voucher is of that type
-
-        :param voucher: dict of a single voucher's data from Acteol
-        :param current_datetime: an Arrow datetime obj
-        :return: dict of expired voucher data mapped for Bink, or None
-        """
-
-        expiry: Optional[str] = voucher.get("ExpiryDate")
-        if expiry and arrow.get(expiry) < current_datetime:
-            return Voucher(
-                state=voucher_state_names[VoucherState.EXPIRED],
-                code="----------",
-                target_value=None,  # None == will be set to Earn Target Value in Hermes
-                value=None,  # None == will be set to Earn Target Value in Hermes
-                issue_date=arrow.get(voucher["URD"]).int_timestamp,
-                expiry_date=arrow.get(voucher["ExpiryDate"]).int_timestamp,
-            )
-
-        return None
-
-    def _make_redeemed_voucher(self, voucher: dict) -> Optional[Voucher]:
-        """
-        Make a Bink redeemed voucher dict if the Acteol voucher is of that type
-
-        :param voucher: dict of a single voucher's data from Acteol
-        :return: dict of redeemed voucher data mapped for Bink, or None
-        """
-
-        if voucher.get("Redeemed") and voucher.get("RedemptionDate"):
-            return Voucher(
-                state=voucher_state_names[VoucherState.REDEEMED],
-                code="----------",
-                target_value=None,  # None == will be set to Earn Target Value in Hermes
-                value=None,  # None == will be set to Earn Target Value in Hermes
-                issue_date=arrow.get(voucher["URD"]).int_timestamp,
-                redeem_date=arrow.get(voucher["RedemptionDate"]).int_timestamp,
-                expiry_date=arrow.get(voucher["ExpiryDate"]).int_timestamp,
-            )
-
-        return None
