d8082f71003c09792083a89eae146a68fbe11c87
mmarsh <mmarsh@bink.com>
2023-08-10 09:58:51 +0100

    WIP itsu join with errors - tests need to be added

--- app/tests/unit/test_itsu_pepper_join.py
+++ app/tests/unit/test_itsu_pepper_join.py
@@ -1,11 +1,15 @@
 import json
 from http import HTTPStatus
-# from unittest.mock import patch
 
 import httpretty
 from soteria.configuration import Configuration
 
 import settings
+
+# from unittest.mock import patch
+
+
+
 # from app.agents.itsu import Itsu
 
 SECRET_ITSU_ACTEOL_JOIN = [{"value": {"password": "MBX1pmb2uxh5vzc@ucp", "username": "acteol.itsu.test@bink.com"}}]
