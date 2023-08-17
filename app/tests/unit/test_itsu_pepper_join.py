3a5e274f92a991be1b29b07f0ff2bbd994627c08
mmarsh <mmarsh@bink.com>
2023-08-17 02:42:31 +0100

    WIP itsu join with errors - tests need to be added

--- app/tests/unit/test_itsu_pepper_join.py
+++ app/tests/unit/test_itsu_pepper_join.py
@@ -3,9 +3,9 @@ from http import HTTPStatus
 from unittest.mock import patch
 
 import httpretty
+import pytest
 from soteria.configuration import Configuration
 
-import settings
 from app.agents.itsu import Itsu
 
 SECRET_ITSU_ACTEOL_JOIN = [{"value": {"password": "MBX1pmb2uxh5vzc@ucp", "username": "acteol.itsu.test@bink.com"}}]
@@ -84,62 +84,89 @@ EXPECTED_PEPPER_PAYLOAD = {
 }
 
 
-def mock_itsu_config():
-    uri = f"{settings.CONFIG_SERVICE_URL}/configuration"
-    print(uri)
-    httpretty.register_uri(
-        httpretty.GET,
-        uri=uri,
-        status=HTTPStatus.OK,
-        body=json.dumps(CONFIG_JSON_ITSU_BODY),
-        content_type="text/json",
-    )
+EXPECTED_ITSU_PEPPER_HEADERS = {
+    "Content-Type": "application/json",
+    "Authorization": "Token 627b9ee5c0aebf0e54a3b3dc",
+    "x-api-version": "10",
+    "x-application-id": "bink-03dxtvc8",
+    "x-client-platform": "BINK",
+}
 
 
-def mock_pepper_config():
-    uri = f"{settings.CONFIG_SERVICE_URL}/configuration"
-    print(uri)
-    httpretty.register_uri(
-        httpretty.GET,
-        uri=uri,
-        status=HTTPStatus.OK,
-        body=json.dumps(CONFIG_JSON_PEPPER_BODY),
-        content_type="text/json",
-    )
-
-
+@pytest.fixture
 @httpretty.activate
-@patch("settings.CONFIG_SERVICE_URL", "http://mock_europa.com")
-def test_itsu_pepper_get_by_id():
-    user_info = {
-        "scheme_account_id": 1235,
-        "channel": "test",
-        "journey_type": Configuration.JOIN_HANDLER,
-        "credentials": {
-            "first_name": "Fake",
-            "last_name": "Name",
-            "email": "test_mm_1@bink.com",
-            "password": "userspassword1",
-            "consents": [{"id": 11738, "slug": "email_marketing", "value": True, "created_on": "1996-09-26T00:00:00"}],
-        },
-    }
-    mock_itsu_config()
-    with patch("app.agents.base.Configuration.get_security_credentials", return_value=SECRET_ITSU_ACTEOL_JOIN):
-        itsu = Itsu(5, user_info, scheme_slug="itsu")
-        print("itsu set up")
-        payload = itsu.pepper_add_user_payload()
-        assert payload == EXPECTED_PEPPER_PAYLOAD
-        mock_pepper_config()
+def itsu():
+    mock_europa_url = "http://mock_europa.com"
+    with patch("settings.CONFIG_SERVICE_URL", mock_europa_url):
+        uri = f"{mock_europa_url}/configuration"
+        user_info = {
+            "scheme_account_id": 1235,
+            "channel": "test",
+            "journey_type": Configuration.JOIN_HANDLER,
+            "credentials": {
+                "first_name": "Fake",
+                "last_name": "Name",
+                "email": "test_mm_1@bink.com",
+                "password": "userspassword1",
+                "consents": [
+                    {"id": 11738, "slug": "email_marketing", "value": True, "created_on": "1996-09-26T00:00:00"}
+                ],
+            },
+        }
+        httpretty.register_uri(
+            httpretty.GET,
+            uri=uri,
+            status=HTTPStatus.OK,
+            body=json.dumps(CONFIG_JSON_ITSU_BODY),
+            content_type="text/json",
+        )
+        with patch("app.agents.base.Configuration.get_security_credentials", return_value=SECRET_ITSU_ACTEOL_JOIN):
+            return Itsu(5, user_info, scheme_slug="itsu")
+
+
+@pytest.fixture
+@httpretty.activate
+def pepper_base_url(itsu):
+    mock_europa_url = "http://mock_europa.com"
+    with patch("settings.CONFIG_SERVICE_URL", mock_europa_url):
         with patch("app.agents.base.Configuration.get_security_credentials", return_value=SECRET_ITSU_PEPPER_JOIN):
+            uri = "http://mock_europa.com/configuration"
+            httpretty.register_uri(
+                httpretty.GET,
+                uri=uri,
+                status=HTTPStatus.OK,
+                body=json.dumps(CONFIG_JSON_PEPPER_BODY),
+                content_type="text/json",
+            )
             pepper_base_url = itsu.set_pepper_config()
-            print("pepper set up")
-            assert pepper_base_url == "https://beta-api.pepperhq.com"
-            httpretty.disable()
+            return pepper_base_url
 
-            pepper_id = itsu.pepper_add_user(pepper_base_url)
-            assert pepper_id == "64d498865e2b5f4d03c2a70e"
 
-            card_number = itsu.call_pepper_for_card_number(pepper_id, pepper_base_url)
-            assert card_number == "1105763052"
+def test_itsu_pepper_get_by_id(itsu):
+    payload = itsu.pepper_add_user_payload()
+    assert payload == EXPECTED_PEPPER_PAYLOAD
+    assert itsu.headers == {}
 
-    assert True
+
+def test_itsu_set_pepper_config(pepper_base_url, itsu):
+    assert pepper_base_url == "https://beta-api.pepperhq.com"
+    payload = itsu.pepper_add_user_payload()
+    assert payload == EXPECTED_PEPPER_PAYLOAD
+    assert itsu.scheme_slug == "itsu"
+    assert itsu.headers == EXPECTED_ITSU_PEPPER_HEADERS
+
+
+"""
+    mock_pepper_config()
+    with patch("app.agents.base.Configuration.get_security_credentials", return_value=SECRET_ITSU_PEPPER_JOIN):
+        pepper_base_url = itsu.set_pepper_config()
+        print("pepper set up")
+        assert pepper_base_url == "https://beta-api.pepperhq.com"
+        httpretty.disable()
+
+        pepper_id = itsu.pepper_add_user(pepper_base_url)
+        assert pepper_id == "64d498865e2b5f4d03c2a70e"
+
+        card_number = itsu.call_pepper_for_card_number(pepper_id, pepper_base_url)
+        assert card_number == "1105763052"
+"""
