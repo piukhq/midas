877435ca035625dbdd377ca236c2305c93690b69
Bilal Leadbitter <bleadbitter@bink.com>
2023-08-23 15:27:32 +0100

    feat: :sparkles: Slight changes to func for balance request

--- app/agents/slimchickens.py
+++ app/agents/slimchickens.py
@@ -12,7 +12,6 @@ from app.agents.schemas import Balance, Voucher
 from app.exceptions import AccountAlreadyExistsError, BaseError, CardNumberError, ConfigurationError, WeakPassword
 from app.reporting import get_logger
 from app.vouchers import VoucherState, voucher_state_names
-from app.exceptions import (CardNumberError, UnknownError)
 
 RETRY_LIMIT = 3
 log = get_logger("slim-chickens")
