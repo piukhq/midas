93607e14ad752bf81944833c30ce08e4d98c2797
Bilal Leadbitter <bleadbitter@bink.com>
2023-08-30 14:05:54 +0100

    feat: isort fix

--- app/agents/slimchickens.py
+++ app/agents/slimchickens.py
@@ -12,8 +12,6 @@ from app.agents.schemas import Balance, Voucher
 from app.exceptions import AccountAlreadyExistsError, BaseError, CardNumberError, ConfigurationError, WeakPassword
 from app.reporting import get_logger
 from app.vouchers import VoucherState, voucher_state_names
-from app.exceptions import CardNumberError
-from requests.models import Response
 
 RETRY_LIMIT = 3
 log = get_logger("slim-chickens")
