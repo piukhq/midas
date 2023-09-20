ed55aa7a194dd04d28b39f5b261fe1d91c93fda2
Michal Jozwiak <jozw.michal@gmail.com>
2023-09-20 15:06:42 +0100

    Feature/ret 2488 stonegate join happy path (#226)
    * add stonegate agent
    
    * add stonegate agent
    
    * fix return type for function
    
    * add stonegate agent
    
    * add unit tests
    
    * add unit tests
    
    * add another test
    
    * finish error handling and tests
    
    * fix test case
    
    * check condition check
    
    * change slug from mixr to stonegate
    
    * change condition check to only check for whether membernumber is empty
    
    ---------
    
    Co-authored-by: Michal Jozwiak <mjozwiak@bink.com>

--- app/agents/stonegate.py
+++ app/agents/stonegate.py
@@ -1,4 +1,3 @@
-# from typing import Tuple
 from urllib.parse import urlencode, urljoin
 
 import argon2
@@ -6,8 +5,8 @@ from blinker import signal
 from soteria.configuration import Configuration
 
 from app.agents.acteol import Acteol
-from app.agents.schemas import Transaction
-from app.exceptions import AccountAlreadyExistsError, BaseError
+from app.agents.schemas import Balance, Transaction
+from app.exceptions import AccountAlreadyExistsError, BaseError, JoinError
 
 hasher = argon2.PasswordHasher()
 
@@ -36,35 +35,25 @@ class Stonegate(Acteol):
             self._oauth_authentication()
         self.headers["Content-Type"] = "application/json"
 
-    def _check_response_for_error(self, resp_json: dict):
-        pass
-
-    def _find_customer_details(self, send_audit: bool = False) -> bool:
-        self.authenticate()
+    def _check_customer_exists(self, send_audit: bool = False) -> bool:
         api_url = urljoin(self.base_url, "api/Customer/FindCustomerDetails")
         payload = {"SearchFilters": {"Email": self.credentials["email"]}}
-        resp = self.make_request(api_url, method="post", timeout=self.API_TIMEOUT, audit=send_audit, json=payload)
+        resp = self.make_request(api_url, method="post", audit=send_audit, json=payload)
         resp_json = resp.json()
-        resp_data = resp_json["ResponseData"][0]
-        errors = resp_data.get("Errors")
+        errors = resp_json.get("Errors")
         if not errors:
             return True
         if errors[0]["ErrorCode"] == 4:
             return False
         else:
-            raise Exception()
+            raise JoinError()
 
-    def join(self):
-        check_user_exists = self._find_customer_details()
-        if check_user_exists:
-            raise AccountAlreadyExistsError()
-
+    def _get_join_payload(self):
         hashed_password = hasher.hash(self.credentials["password"])
         marketing_optin = False
         if consents := self.credentials.get("consents", []):
             marketing_optin = consents[0]["value"]
-        url = urljoin(self.base_url, "Customer/Post")
-        payload = {
+        return {
             "PersonalInfo": {
                 "FirstName": self.credentials["first_name"],
                 "LastName": self.credentials["last_name"],
@@ -81,17 +70,35 @@ class Stonegate(Acteol):
             ],
             "MemberNumber": {"GenerateMemberNumber": "true"},
         }
+
+    def join(self):
+        self.authenticate()
+        if self._check_customer_exists():
+            raise AccountAlreadyExistsError()
+
+        url = urljoin(self.base_url, "api/Customer/Post")
+        payload = self._get_join_payload()
         try:
             resp = self.make_request(url, method="post", audit=True, json=payload)
             resp_json = resp.json()
-            self._check_response_for_error(resp_json)
+            if not resp_json["ResponseData"]["MemberNumber"]:
+                raise JoinError()
             self.identifier = {"merchant_identifier": resp_json["ResponseData"]["MemberNumber"]}
-            signal("join-success").senf(self, slug=self.scheme_slug, channel=self.channel)
+            signal("join-success").send(self, slug=self.scheme_slug, channel=self.channel)
         except BaseError as ex:
             signal("join-fail").send(self, slug=self.scheme_slug, channel=self.channel)
-            error_code = ex.exception.response.status_code if ex.exception.response is not None else ex.code
-            self.handle_error_codes(error_code)
+            raise ex
+
+    def login(self):
+        pass
+
+    def balance(self):
+        return Balance(
+            points=0,
+            value=0,
+            value_label="",
+            vouchers=[],
+        )
 
     def transactions(self) -> list[Transaction]:
-        # No transactions available for Itsu, return empty list to prevent exception being raised.
         return []
