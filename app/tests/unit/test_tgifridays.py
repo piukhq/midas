a88a4972e41f1c105028737063e82fefdc0e94a6
Carla Gouws <cgouws@bink.com>
2024-01-31 10:27:48 +0000

    TGI-Fridays with exception handling, signalling and data auditing

---
+++ app/tests/unit/test_tgifridays.py
@@ -0,0 +1,11 @@
+
+
+
+CREDENTIALS = {
+    "client": "0000000000000000000",
+    "last_name": "Smith",
+    "first_name": "John",
+    "email": "johnsmith@test.com",
+    "password": "password",
+    "title": "mr",
+}
